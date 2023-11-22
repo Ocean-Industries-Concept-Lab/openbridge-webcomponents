986ffa74a629e6391041387a94e045f0b1f8ec7c
Torstein I. Bø <361267+tibnor@users.noreply.github.com>
2023-11-22 22:19:05 +0100

    feat: alert button

---
+++ packages/openbridge-webcomponents/src/ApplicationComponents/AlertButton.stories.ts
@@ -0,0 +1,105 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { AlertButton, AlertType } from './AlertButton';
+import './AlertButton';
+
+const meta: Meta<typeof AlertButton> = {
+  title: 'Application/Alert button',
+  tags: ['autodocs'],
+  component: "ob-alert-button",
+  args: {
+    nAlerts: 2,
+    counter: true,
+    standalone: false,
+  },
+  argTypes: {
+    nAlerts: {
+        control: { type: 'number', min: 0, max: 99 },
+        },
+    counter: {
+        control: { type: 'boolean'},
+        },
+    standalone: {
+            control: { type: 'boolean'},
+            },
+    type: {
+        options: Object.values(AlertType),
+        control: { type: 'select' },
+        },
+
+  },
+} satisfies Meta<AlertButton>;
+
+export default meta;
+type Story = StoryObj<AlertButton>;
+
+export const Alarm: Story = {
+    args: {
+        type: AlertType.Alarm
+    },
+};
+
+export const Warning: Story = {
+    args: {
+        type: AlertType.Warning,
+    },
+};
+
+export const Caution: Story = {
+    args: {
+        type: AlertType.Caution
+    },
+};
+
+export const Running: Story = {
+    args: {
+        type: AlertType.Running
+    },
+};
+
+export const Command: Story = {
+    args: {
+        type: AlertType.Command
+    },
+};
+
+export const Notification: Story = {
+    args: {
+        type: AlertType.Notification
+    }
+};
+
+export const Regular: Story = {
+    args: {
+        type: AlertType.Regular
+    }
+};
+
+export const Flat: Story = {
+    args: {
+      type: AlertType.Flat
+    },
+  };
+
+export const RegularNoCounter: Story = {
+    args: {
+        type: AlertType.Regular,
+        counter: false
+    }
+};
+
+export const RegularStandalone: Story = {
+    args: {
+        type: AlertType.Regular,
+        counter: true,
+        standalone: true
+    }
+};
+
+export const RegularStandaloneNoCounter: Story = {
+    args: {
+        type: AlertType.Regular,
+        counter: false,
+        standalone: true
+    }
+};
+
