caf5e62751de92b21bf628cc87673f27924b14cc
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2023-11-26 20:41:10 +0000

    chore: simplifiy style mixin

--- packages/openbridge-webcomponents/src/icon/Icon.ts
+++ packages/openbridge-webcomponents/src/icon/Icon.ts
@@ -1,50 +1,41 @@
-import { LitElement, html, css, TemplateResult } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
-import {until} from 'lit-html/directives/until.js';
-import { unsafeSVG } from "lit/directives/unsafe-svg.js"
-import { iconIds } from '../icons';
+import { LitElement, html, TemplateResult, unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { until } from "lit-html/directives/until.js";
+import { unsafeSVG } from "lit/directives/unsafe-svg.js";
+import { iconIds } from "../icons";
+import componentStyle from "./Icon.css?inline";
 
-@customElement('ob-icon')
+@customElement("ob-icon")
 export class Icon extends LitElement {
-    @property({ type: String }) icon = '01-placeholder'
-    @property({ type: Number }) size = 24;
+  @property({ type: String }) icon = "01-placeholder";
+  @property({ type: Number }) size = 24;
 
-    async iconSvg(icon: string): Promise<TemplateResult> {
-        if (!iconIds.includes(icon)) {
-            console.warn(`Icon ${icon} not found. Using placeholder instead.`)
-            icon = '01-placeholder'
-        }
-
-        const svg = await import(`../assets/icons/${icon}.svg?raw`);
-        return html`${unsafeSVG(svg.default)}`
+  async iconSvg(icon: string): Promise<TemplateResult> {
+    if (!iconIds.includes(icon)) {
+      console.warn(`Icon ${icon} not found. Using placeholder instead.`);
+      icon = "01-placeholder";
     }
-    
-    render() {
-        return html`
-        <div class="wrapper" style="--size:${this.size}px">
+
+    const svg = await import(`../assets/icons/${icon}.svg?raw`);
+    return html`${unsafeSVG(svg.default)}`;
+  }
+
+  render() {
+    return html`
+      <div class="wrapper" style="--size:${this.size}px">
         ${until(
-            this.iconSvg(this.icon).then(res => html`${res}`),
-            html`Loading...`,
-          )}
-        </div>
-    `
-    }
+          this.iconSvg(this.icon).then((res) => html`${res}`),
+          html`Loading...`
+        )}
+      </div>
+    `;
+  }
 
-    static styles = css`
-     .wrapper {
-        height: var(--size);
-        width: var(--size);
-
-        & > * {
-            height: 100%;
-            width: 100%;
-        }
-     }`
-        ;
+  static styles = unsafeCSS(componentStyle);
 }
 
 declare global {
-    interface HTMLElementTagNameMap {
-        'ob-icon': Icon
-    }
+  interface HTMLElementTagNameMap {
+    "ob-icon": Icon;
+  }
 }
