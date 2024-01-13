4797dc4426bbbcdd21334846e8cd48e1f22e3a36
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2024-01-13 12:49:12 +0000

    vertical scrollbar

---
+++ packages/openbridge-webcomponents/src/components/scrollbar/scrollbar.ts
@@ -0,0 +1,23 @@
+import { LitElement, html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+import compentStyle from "./scrollbar.style";
+
+@customElement('obc-scrollbar')
+export class ObcScrollbar extends LitElement {
+
+ override render() {
+    return html`
+        <div class="wrapper">
+          <slot></slot>
+        </div>
+    `
+  }
+
+  static override styles = compentStyle;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'obc-scrollbar': ObcScrollbar
+  }
+}
