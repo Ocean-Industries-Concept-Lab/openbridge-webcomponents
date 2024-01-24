a6cfb7796eae32e079ca71a43551825fb8b8c075
Torstein I. Bø <torsteinibo@gmail.com>
2024-01-24 09:49:45 +0100

    Firebase deploy and shared session (#28)

--- packages/openbridge-webcomponents-vue/src/components/brilliance-menu/ObcBrillianceMenu.vue
+++ packages/openbridge-webcomponents-vue/src/components/brilliance-menu/ObcBrillianceMenu.vue
@@ -1,27 +1,50 @@
 
     <script setup lang="ts">
-      import { h, useSlots } from "vue";
+      import { h, useSlots, reactive } from "vue";
       import { assignSlotNodes, Slots } from "@lit-labs/vue-utils/wrapper-utils.js";
       import 'openbridge-webcomponents/dist/components/brilliance-menu/brilliance-menu.js';
       
 
       export interface Props {
-     
+     palette?: string
    }
 
       
+  const vueProps = defineProps<Props>();
 
-      
+  const defaults = reactive({} as Props);
+  const vDefaults = {
+    created(el: any) {
+      for (const p in vueProps) {
+        defaults[p as keyof Props] = el[p];
+      }
+    }
+  };
+
+  let hasRendered = false;
+
+      const emit = defineEmits<{
+        (e: 'palette-changed', payload: CustomEvent<unknown>): void
+      }>();
 
       const slots = useSlots();
 
       const render = () => {
         const eventProps = {
-    
+    onPaletteChanged: (event: CustomEvent<unknown>) => emit('palette-changed', event as CustomEvent<unknown>)
   };
         const props = eventProps as (typeof eventProps & Props);
 
         
+      for (const p in vueProps) {
+        const v = vueProps[p as keyof Props];
+        if ((v !== undefined) || hasRendered) {
+          (props[p as keyof Props] as unknown) = v ?? defaults[p as keyof Props];
+        }
+      }
+
+      hasRendered = true;
+    
 
         return h(
           'obc-brilliance-menu',
@@ -30,4 +53,4 @@
         );
       };
     </script>
-    <template><render /></template>
+    <template><render v-defaults /></template>
--- packages/vue-demo/src/components/DemoAlertMenu.vue
+++ packages/vue-demo/src/components/DemoAlertMenu.vue
@@ -1,35 +1,44 @@
 <script setup lang="ts">
-import { useAlertStore } from '@/stores/alert';
-import AlertMenu from "openbridge-webcomponents-vue/components/alert-menu/ObcAlertMenu";
-import AlertMenuItem from "openbridge-webcomponents-vue/components/alert-menu-item/ObcAlertMenuItem";
-import { useRouter } from 'vue-router';
+import { useAlertStore } from '@/stores/alert'
+import AlertMenu from 'openbridge-webcomponents-vue/components/alert-menu/ObcAlertMenu'
+import AlertMenuItem from 'openbridge-webcomponents-vue/components/alert-menu-item/ObcAlertMenuItem'
+import { useRouter } from 'vue-router'
 
-const model = defineModel<boolean>();
+const model = defineModel<boolean>()
 
-const alertStore = useAlertStore();
+const alertStore = useAlertStore()
 
-const router = useRouter();
-
+const router = useRouter()
 
 function onAlertListClick() {
-    router.push({ name: 'alert' });
-    if (model.value) {
-        model.value = false;
-    }
+  router.push({ name: 'alert' })
+  if (model.value) {
+    model.value = false
+  }
 }
-
-
 </script>
 
 <template>
-    <AlertMenu v-if="model" class="alert-menu" @ack-all-click="alertStore.ackAllAlerts"
-        @alert-list-click="onAlertListClick" 
-        :empty="alertStore.activeAlerts.length === 0">
-        <AlertMenuItem v-for="a of alertStore.activeAlerts" :key="a.tag" :message="a.cause" :time="a.time.toISOString()"
-            time-since="1h 2m" :alert-type="a.alertType" acknowledgeble @ack-click="() => a.alertStatus = 'acked'">
-            <template #icon>
-                <obi-14-alarm-unack use-css-color></obi-14-alarm-unack>
-            </template>
-        </AlertMenuItem>
-    </AlertMenu>
+  <AlertMenu
+    v-if="model"
+    class="alert-menu"
+    @ack-all-click="alertStore.ackAllAlerts"
+    @alert-list-click="onAlertListClick"
+    :empty="alertStore.activeAlerts.length === 0"
+  >
+    <AlertMenuItem
+      v-for="a of alertStore.activeAlerts"
+      :key="a.tag"
+      :message="a.cause"
+      :time="a.time.toISOString()"
+      time-since="1h 2m"
+      :alert-type="a.alertType"
+      acknowledgeble
+      @ack-click="() => (a.alertStatus = 'acked')"
+    >
+      <template #icon>
+        <obi-14-alarm-unack use-css-color></obi-14-alarm-unack>
+      </template>
+    </AlertMenuItem>
+  </AlertMenu>
 </template>
