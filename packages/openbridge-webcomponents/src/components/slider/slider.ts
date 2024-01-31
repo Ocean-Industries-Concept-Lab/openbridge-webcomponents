e716015248ae835eb45bb8f8bb518ed323e3905f
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2024-01-31 12:24:22 +0000

    fix: range

--- packages/openbridge-webcomponents/src/components/slider/slider.ts
+++ packages/openbridge-webcomponents/src/components/slider/slider.ts
@@ -1,5 +1,7 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import {styleMap} from 'lit/directives/style-map.js';
+import {ifDefined} from 'lit/directives/if-defined.js';
 import componentStyle from './slider.style';
 import '../icon-button/icon-button';
 
@@ -13,23 +15,30 @@ export class ObcSlider extends LitElement {
   @property({type: Number}) value = 50;
   @property({type: Number}) min = 0;
   @property({type: Number}) max = 100;
-  @property({type: Number}) step = 1;
+  @property({type: Number}) step: number | undefined;
 
   override render() {
+    const ratio = (this.value - this.min) / (this.max - this.min);
     return html`
       <slot name="icon-left" class="icon"> </slot>
-      <input
-        type="range"
-        min="${this.min}"
-        max="${this.max}"
-        step=${this.step}
-        value=${this.value}
-        class="slider"
-        @change=${(event: Event) => {
-          this.value = Number((event.target as HTMLInputElement).value);
-          this.dispatchEvent(new CustomEvent('value', {detail: this.value}));
-        }}
-      />
+      <div class="wrapper">
+        <div
+          class="interactive-track"
+          style=${styleMap({'--ratio': ratio})}
+        ></div>
+        <input
+          type="range"
+          min="${this.min}"
+          max="${this.max}"
+          step=${ifDefined(this.step)}
+          value=${this.value}
+          class="slider"
+          @input=${(event: Event) => {
+            this.value = Number((event.target as HTMLInputElement).value);
+            this.dispatchEvent(new CustomEvent('value', {detail: this.value}));
+          }}
+        />
+      </div>
       <slot name="icon-right" class="icon"></slot>
     `;
   }
