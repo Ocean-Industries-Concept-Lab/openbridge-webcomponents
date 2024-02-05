a216526676b95875c0a3efdd64c46af64d219292
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2024-02-05 12:59:37 +0000

    fix format

--- packages/openbridge-webcomponents/src/components/alert-menu/alert-menu.stories.ts
+++ packages/openbridge-webcomponents/src/components/alert-menu/alert-menu.stories.ts
@@ -108,13 +108,13 @@ export const Narrow: Story = {
 
 export const Empty: Story = {
   args: {},
-  render: (args) => html`
-  <style>
+  render: (args) =>
+    html` <style>
         obc-alert-menu {
           width: ${args.narrow ? '500px' : '800px'};
           height: 500px;
           display: block;
         }
       </style>
-  <obc-alert-menu empty></obc-alert-menu>`,
+      <obc-alert-menu empty></obc-alert-menu>`,
 };
