eb17b9360455bfeca125de92de130b6906f17616
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2024-02-15 10:14:19 +0000

    fix: remove flash

--- packages/vue-demo/src/views/InstrumentDemo.vue
+++ packages/vue-demo/src/views/InstrumentDemo.vue
@@ -1,27 +1,32 @@
 <script setup lang="ts">
-import ObcAzimuthThruster from '@tibnor/openbridge-webcomponents-vue/navigation-instruments/azimuth-thruster/ObcAzimuthThruster';
-import { onMounted, ref } from 'vue';
+import ObcAzimuthThruster from '@tibnor/openbridge-webcomponents-vue/navigation-instruments/azimuth-thruster/ObcAzimuthThruster'
+import { onMounted, ref } from 'vue'
 
 const angle = ref(30)
 const thrust = ref(50)
 
 onMounted(() => {
-    const startTime = Date.now()
-    setInterval(() => {
-        const seconds = (Date.now() - startTime) / 1000
-        angle.value = Math.sin(seconds / 2) * 30
-        thrust.value = Math.sin(seconds / 3) * 50 + 50
-    }, 1000/60)
+  const startTime = Date.now()
+  setInterval(() => {
+    const seconds = (Date.now() - startTime) / 1000
+    angle.value = Math.sin(seconds / 2) * 30
+    thrust.value = Math.sin(seconds / 3) * 50 + 50
+  }, 1000 / 60)
 })
-
-
 </script>
 
 <template>
-    <div class="container">
-        <!-- 10 copies -->
-  <ObcAzimuthThruster class="instrument" :widthPx="300" :angle="angle" :thrust="thrust" v-for="i in 10" :key="i" />
-</div>
+  <div class="container">
+    <!-- 10 copies -->
+    <ObcAzimuthThruster
+      class="instrument"
+      :widthPx="300"
+      :angle="angle"
+      :thrust="thrust"
+      v-for="i in 10"
+      :key="i"
+    />
+  </div>
 </template>
 
 <style scoped>
@@ -32,15 +37,13 @@ onMounted(() => {
   width: 100%;
   height: 100%;
   flex-wrap: wrap;
-  background-color: var(--Container-Background-color, #F7F7F7);;
+  background-color: var(--Container-Background-color, #f7f7f7);
 }
 
 .instrument {
-    display: block;
-    width: 300px;
+  display: block;
+  width: 300px;
   height: 300px;
   min-width: 300px;
 }
-
 </style>
-
--- packages/vue-demo/src/views/ResponsiveInstrumentDemo.vue
+++ packages/vue-demo/src/views/ResponsiveInstrumentDemo.vue
@@ -1,32 +1,49 @@
 <script setup lang="ts">
-import ObcAzimuthThruster from '@tibnor/openbridge-webcomponents-vue/navigation-instruments/azimuth-thruster/ObcAzimuthThruster';
-import { onMounted, ref } from 'vue';
-import { useElementSize } from '@vueuse/core';
+import ObcAzimuthThruster from '@tibnor/openbridge-webcomponents-vue/navigation-instruments/azimuth-thruster/ObcAzimuthThruster'
+import { onMounted, ref } from 'vue'
+import { useElementSize } from '@vueuse/core'
 
 const angle = ref(30)
 const thrust = ref(50)
 const angleSetpoint = ref(0)
 
+const utcOffsetHours = new Date().getTimezoneOffset() / 60
+
 onMounted(() => {
-    setInterval(() => {
-        const time = Date.now()
-        const clockHours = (time / 1000 / 60 / 60) % 12
-        const clockMinutes = (time / 1000 / 60) % 60
-        const clockSeconds = (time / 1000) % 60
-        angle.value = clockHours / 12 * 360
-        angleSetpoint.value = clockMinutes / 60 * 360
-        thrust.value = (100-clockSeconds / 30 * 100)
-    }, 1000/60)
+  updateTime();
+  setInterval(updateTime, 1000 / 60)
 })
+
+function updateTime() {
+  const time = Date.now()
+    const clockHours = (time / 1000 / 60 / 60) % 12 - utcOffsetHours
+    const clockMinutes = (time / 1000 / 60) % 60
+    let clockSeconds = (time / 1000) % 60
+    angle.value = (clockHours / 12) * 360
+    angleSetpoint.value = (clockMinutes / 60) * 360
+    // map seconds so that the first minute it is increasing and the following minuite it is decreasing
+    if (clockMinutes % 2 < 1) {
+      clockSeconds = 60 - clockSeconds
+    }
+
+    // map seconds to -100 to 100 range
+    thrust.value = 100 - (clockSeconds / 30) * 100
+}
 
 const thruster = ref<HTMLElement | null>(null)
 const { width } = useElementSize(thruster)
-
 </script>
 
 <template>
   <div class="container">
-    <ObcAzimuthThruster ref="thruster" class="instrument" :widthPx="width" :angle="angle" :thrust="thrust" :angle-setpoint="angleSetpoint"/>
+    <ObcAzimuthThruster
+      ref="thruster"
+      class="instrument"
+      :widthPx="width"
+      :angle="angle"
+      :thrust="thrust"
+      :angle-setpoint="angleSetpoint"
+    />
   </div>
 </template>
 
@@ -47,4 +64,3 @@ const { width } = useElementSize(thruster)
   height: 100%;
 }
 </style>
-
