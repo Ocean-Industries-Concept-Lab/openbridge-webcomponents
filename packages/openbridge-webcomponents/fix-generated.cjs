097eca575366b0b09d4ef6d8f3ada1eb6798360f
Torstein A. Bø <torstein.bo@sintef.no>
2024-03-08 20:06:59 +0000

    started with release it

--- packages/openbridge-webcomponents/fix-generated.cjs
+++ packages/openbridge-webcomponents/fix-generated.cjs
@@ -1,5 +1,10 @@
 const fs = require('fs');
 
+const releaseItVersion = require('./package.json').devDependencies['release-it'];
+const releaseItBumperVersion = require('./package.json').devDependencies['@release-it/bumper'];
+
+console.log('releaseItVersion', releaseItVersion);
+
 function fixIndexFiles() {
   const indexFiles = [
     '../openbridge-webcomponents-vue/src/components/table/table.ts',
@@ -20,6 +25,16 @@ function addRepositoryToPackageJsonVue() {
     url: 'git+https://github.com/Ocean-Industries-Concept-Lab/openbridge-webcomponents.git',
     directory: 'packages/openbridge-webcomponents-vue',
   };
+  packageJson.scripts = {
+    ...packageJson.scripts,
+    "release": "release-it",
+    "@release-it/bumper": releaseItBumperVersion,
+  }
+  packageJson.devDependencies = {
+    ...packageJson.devDependencies,
+    "release-it": releaseItVersion
+  }
+
   // add license
   packageJson.license = 'Apache-2.0';
   fs.writeFileSync(
@@ -36,6 +51,15 @@ function addRepositoryToPackageJsonReact() {
     directory: 'packages/openbridge-webcomponents-react',
   };
   packageJson.license = 'Apache-2.0';
+  packageJson.scripts = {
+    ...packageJson.scripts,
+    "release": "release-it"
+  }
+  packageJson.devDependencies = {
+    ...packageJson.devDependencies,
+    "release-it": releaseItVersion,
+    "@release-it/bumper": releaseItBumperVersion,
+  }
   fs.writeFileSync(
     '../openbridge-webcomponents-react/package.json',
     JSON.stringify(packageJson, null, 2)
