f80d1c0c2f12f50afc24a86e30515d912d4f0619
Torstein A. Bø <torstein.bo@sintef.no>
2024-03-14 08:15:36 +0000

    fix

--- packages/openbridge-webcomponents-vue/icons/Obi07TargetRadar.d.ts
+++ packages/openbridge-webcomponents-vue/icons/Obi07TargetRadar.d.ts
@@ -2,7 +2,7 @@ import '@oicl/openbridge-webcomponents/dist/icons/icon-07-target-radar.js';
 export interface Props {
     useCssColor?: boolean;
 }
-declare const _default: import("vue").DefineComponent<__VLS_TypePropsToRuntimeProps<Props>, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<__VLS_TypePropsToRuntimeProps<Props>>>, {}, {}>;
+declare const _default: import("vue").DefineComponent<__VLS_TypePropsToRuntimeProps<Props>, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<__VLS_TypePropsToRuntimeProps<Props>>>, {}, {}>;
 export default _default;
 type __VLS_NonUndefinedable<T> = T extends undefined ? never : T;
 type __VLS_TypePropsToRuntimeProps<T> = {
