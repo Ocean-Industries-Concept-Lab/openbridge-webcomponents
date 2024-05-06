c37a2b5a903307ef6124906093768db1e2ec82ef
Torstein A. Bø <torstein.bo@sintef.no>
2024-05-06 20:01:41 +0000

    feat: readout

--- packages/openbridge-webcomponents/src/automation/automation-readout/automation-readout.stories.ts
+++ packages/openbridge-webcomponents/src/automation/automation-readout/automation-readout.stories.ts
@@ -1,27 +1,21 @@
-import type { Meta, StoryObj } from '@storybook/web-components';
-import { ObcAutomationReadout } from './automation-readout';
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {ObcAutomationReadout} from './automation-readout';
 import './automation-readout';
+import {AutomationReadoutPosition} from './automation-readout';
 import '../horizontal-line/horizontal-line';
+import '../vertical-line/vertical-line';
 import {LineMedium, LineType} from '../index';
-import { html } from 'lit';
+import {html} from 'lit';
 
 const meta: Meta<typeof ObcAutomationReadout> = {
   title: 'Automation/Readout',
   tags: ['autodocs'],
-  component: "obc-automation-readout",
-  args: {
-  },
-} satisfies Meta<ObcAutomationReadout>;
-
-export default meta;
-type Story = StoryObj<ObcAutomationReadout>;
-
-
-export const UsageWithPipe: Story = {
+  component: 'obc-automation-readout',
   args: {
     value: 25,
     unit: '°C',
     numberOfDigits: 3,
+    lineType: LineType.fluid,
   },
   argTypes: {
     value: {
@@ -31,40 +25,172 @@ export const UsageWithPipe: Story = {
         max: 999,
       },
     },
+    lineType: {
+      options: ['fluid', 'electric', 'air', 'connector'],
+      control: {type: 'radio'},
+    },
   },
-  render: (args) => 
-    html`
+} satisfies Meta<ObcAutomationReadout>;
+
+export default meta;
+type Story = StoryObj<ObcAutomationReadout>;
+
+export const UsageWithPipeBottom: Story = {
+  render: (args) => html`
     <style>
-    .canvas {
-      position: relative;
-      width: 400px;
-      height: 400px;
-    }
+      .canvas {
+        position: relative;
+        width: 400px;
+        height: 400px;
+      }
 
-    #line1 {
-      position: absolute;
-      top: 0;
-      left: 0;
-    }
+      #line-bottom {
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
 
-    #readout {
-      position: absolute;
-      top: 12px; /* 12px is the height of the grid size */
-      left: calc(2.5 * 24px); /* 5 is the length of the line */
-    }
+      #readout-bottom {
+        position: absolute;
+        top: 12px; /* 12px is the height of the grid size */
+        left: calc(2.5 * 24px); /* 5 is the length of the line */
+      }
     </style>
     <div class="canvas">
-    <obc-automation-readout
-      .value=${args.value}
-      .unit=${args.unit}
-      .numberOfDigits=${args.numberOfDigits}
-      id="readout"
-    ></obc-automation-readout>
-    <obc-horizontal-line
-          .medium=${LineMedium.water}
-          .line-type=${LineType.fluid}
-          length="5"
-          id="line1"
-        ></obc-horizontal-line>
-    `
-}
+      <obc-automation-readout
+        .value=${args.value}
+        .unit=${args.unit}
+        .numberOfDigits=${args.numberOfDigits}
+        .position=${AutomationReadoutPosition.Bottom}
+        .lineType=${args.lineType}
+        id="readout-bottom"
+      ></obc-automation-readout>
+      <obc-horizontal-line
+        .medium=${LineMedium.water}
+        .lineType=${args.lineType}
+        length="5"
+        id="line-bottom"
+      ></obc-horizontal-line>
+    </div>
+  `,
+};
+
+export const UsageWithPipeTop: Story = {
+  render: (args) => html`
+    <style>
+      .canvas {
+        position: relative;
+        width: 400px;
+        height: 400px;
+      }
+
+      #line-top {
+        position: absolute;
+        top: 24px;
+        left: 0;
+      }
+
+      #readout-top {
+        position: absolute;
+        top: 36px; /* 12px is the height of the grid size */
+        left: calc(2.5 * 24px); /* 5 is the length of the line */
+      }
+    </style>
+    <div class="canvas">
+      <obc-automation-readout
+        .value=${args.value}
+        .unit=${args.unit}
+        .numberOfDigits=${args.numberOfDigits}
+        .position=${AutomationReadoutPosition.Top}
+        .lineType=${args.lineType}
+        id="readout-top"
+      ></obc-automation-readout>
+      <obc-horizontal-line
+        .medium=${LineMedium.water}
+        .lineType=${args.lineType}
+        length="5"
+        id="line-top"
+      ></obc-horizontal-line>
+    </div>
+  `,
+};
+
+export const UsageWithPipeLeft: Story = {
+  render: (args) => html`
+    <style>
+      .canvas {
+        position: relative;
+        width: 400px;
+        height: 400px;
+      }
+
+      #line-left {
+        position: absolute;
+        top: 0;
+        left: calc(24px * 3);
+      }
+
+      #readout-left {
+        position: absolute;
+        top: calc(2.5 * 24px); /* 24px is the height of the grid size */
+        left: calc(24px * 3 + 12px);
+      }
+    </style>
+    <div class="canvas">
+      <obc-automation-readout
+        .value=${args.value}
+        .unit=${args.unit}
+        .numberOfDigits=${args.numberOfDigits}
+        .position=${AutomationReadoutPosition.Left}
+        .lineType=${args.lineType}
+        id="readout-left"
+      ></obc-automation-readout>
+      <obc-vertical-line
+        .medium=${LineMedium.water}
+        .lineType=${args.lineType}
+        length="5"
+        id="line-left"
+      ></obc-vertical-line>
+    </div>
+  `,
+};
+
+export const UsageWithPipeRight: Story = {
+  render: (args) => html`
+    <style>
+      .canvas {
+        position: relative;
+        width: 400px;
+        height: 400px;
+      }
+
+      #line-right {
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+
+      #readout-right {
+        position: absolute;
+        top: calc(2.5 * 24px); /* 24px is the height of the grid size */
+        left: 12px;
+      }
+    </style>
+    <div class="canvas">
+      <obc-automation-readout
+        .value=${args.value}
+        .unit=${args.unit}
+        .numberOfDigits=${args.numberOfDigits}
+        .position=${AutomationReadoutPosition.Right}
+        .lineType=${args.lineType}
+        id="readout-right"
+      ></obc-automation-readout>
+      <obc-vertical-line
+        .medium=${LineMedium.water}
+        .lineType=${args.lineType}
+        length="5"
+        id="line-right"
+      ></obc-vertical-line>
+    </div>
+  `,
+};
--- packages/openbridge-webcomponents/src/automation/automation-readout/automation-readout.ts
+++ packages/openbridge-webcomponents/src/automation/automation-readout/automation-readout.ts
@@ -1,22 +1,54 @@
-import { LitElement, html, unsafeCSS } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
-import compentStyle from "./automation-readout.css?inline";
+import {LitElement, html, unsafeCSS} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import compentStyle from './automation-readout.css?inline';
+import {classMap} from 'lit/directives/class-map.js';
+import {LineType, lineWidth} from '..';
+
+export enum AutomationReadoutPosition {
+  Left = 'left',
+  Right = 'right',
+  Top = 'top',
+  Bottom = 'bottom',
+}
 
 @customElement('obc-automation-readout')
 export class ObcAutomationReadout extends LitElement {
-  @property({ type: Number }) value = 0;
-  @property({ type: String }) unit = '';
-  @property({ type: Number }) numberOfDigits = 3;
+  @property({type: Number}) value = 0;
+  @property({type: String}) unit = '';
+  @property({type: Number}) numberOfDigits = 3;
+  @property({type: String}) position: AutomationReadoutPosition =
+    AutomationReadoutPosition.Bottom;
+  @property({type: String}) lineType: LineType | undefined = undefined;
 
   override render() {
+    const offset = this.getLineWidth() / 2;
+
     return html`
-      <div class="wrapper">
+      <button
+        class=${classMap({
+          wrapper: true,
+          [`position-${this.position}`]: true,
+        })}
+        style="--offset: ${offset}px;"
+      >
         <div class="visible-wrapper">
-          <div class="value" style="min-width: ${this.numberOfDigits.toFixed(0)}ch">${this.value.toFixed(0)}</div>
+          <div
+            class="value"
+            style="min-width: ${this.numberOfDigits.toFixed(0)}ch"
+          >
+            ${this.value.toFixed(0)}
+          </div>
           <div class="unit">${this.unit}</div>
         </div>
-      </div>
-      `
+      </button>
+    `;
+  }
+
+  getLineWidth(): number {
+    if (this.lineType === undefined) {
+      return 0;
+    }
+    return lineWidth(this.lineType);
   }
 
   static override styles = unsafeCSS(compentStyle);
@@ -24,6 +56,6 @@ export class ObcAutomationReadout extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'obc-automation-readout': ObcAutomationReadout
+    'obc-automation-readout': ObcAutomationReadout;
   }
 }
