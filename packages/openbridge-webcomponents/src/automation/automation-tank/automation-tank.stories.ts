87c503be46ac4d0d4033e2de5a0f0afb2a2d6213
Torstein A. Bø <torstein.bo@sintef.no>
2024-05-07 20:03:35 +0000

    including badges and tag

--- packages/openbridge-webcomponents/src/automation/automation-tank/automation-tank.stories.ts
+++ packages/openbridge-webcomponents/src/automation/automation-tank/automation-tank.stories.ts
@@ -1,6 +1,10 @@
 import type {Meta, StoryObj} from '@storybook/web-components';
-import {ObcAutomationTank, TankTrend} from './automation-tank';
+import {ObcAutomationTank, TankTrend, TankVariant} from './automation-tank';
 import './automation-tank';
+import '../../components/badge/badge';
+import '../../icons/icon-08-auto';
+import '../../icons/icon-16-lock';
+import {html} from 'lit';
 
 const meta: Meta<typeof ObcAutomationTank> = {
   title: 'Automation/Tank',
@@ -10,6 +14,7 @@ const meta: Meta<typeof ObcAutomationTank> = {
     value: 9_000,
     max: 10_000,
     trend: TankTrend.fastFalling,
+    tag: '#0000',
   },
   argTypes: {
     trend: {
@@ -19,12 +24,48 @@ const meta: Meta<typeof ObcAutomationTank> = {
     value: {
       control: {type: 'range', min: 0, max: 10_000},
     },
+    variant: {
+      options: Object.values(TankVariant),
+      control: {type: 'radio'},
+    },
   },
+  decorators: [
+    (story) =>
+      html`<div
+        style="display: flex; justify-content: center; align-items: center; height: 100vh"
+      >
+        ${story()}
+      </div>`,
+  ],
 } satisfies Meta<ObcAutomationTank>;
 
 export default meta;
 type Story = StoryObj<ObcAutomationTank>;
 
-export const Primary: Story = {
+export const Vertical: Story = {
   args: {},
+};
+
+export const VerticalWithBadges: Story = {
+  render(args) {
+    return html`<obc-automation-tank
+      .value=${args.value}
+      .max=${args.max}
+      .trend=${args.trend}
+      .tag=${args.tag}
+    >
+      <obc-badge slot="badges" size="regular" hide-number type="automation">
+        <obi-08-auto></obi-08-auto>
+      </obc-badge>
+      <obc-badge slot="badges" size="regular" hide-number type="automation">
+        <obi-16-lock></obi-16-lock>
+      </obc-badge>
+    </obc-automation-tank>`;
+  },
+};
+
+export const Compact: Story = {
+  args: {
+    variant: TankVariant.compact,
+  },
 };
--- packages/openbridge-webcomponents/src/automation/automation-tank/automation-tank.ts
+++ packages/openbridge-webcomponents/src/automation/automation-tank/automation-tank.ts
@@ -8,6 +8,7 @@ import '../../icons/icon-02-chevron-up';
 import '../../icons/icon-02-chevron-double-down';
 import '../../icons/icon-02-chevron-down';
 import '../../icons/icon-01-off';
+import {classMap} from 'lit/directives/class-map.js';
 
 export enum TankTrend {
   fastRising = 'fast-rising',
@@ -17,12 +18,19 @@ export enum TankTrend {
   fastFalling = 'fast-falling',
 }
 
+export enum TankVariant {
+  vertical = 'vertical',
+  compact = 'compact',
+}
+
 @customElement('obc-automation-tank')
 export class ObcAutomationTank extends LitElement {
   @property({type: String}) medium: LineMedium = LineMedium.water;
   @property({type: Number}) value: number = 0;
   @property({type: Number}) max: number = 100;
   @property({type: String}) trend: TankTrend = TankTrend.stable;
+  @property({type: String}) variant: TankVariant = TankVariant.vertical;
+  @property({type: String}) tag: string = '';
 
   trendIcon(): HTMLTemplateResult {
     if (this.trend === TankTrend.fastRising) {
@@ -48,31 +56,44 @@ export class ObcAutomationTank extends LitElement {
     const percent = (this.value / this.max) * 100;
 
     return html`
-      <div class="wrapper" style="--percent: ${percent}%">
-        <div class="bar-container">
-          <div class="bar"></div>
+      <div class="outer">
+        <div class="badges">
+          <slot name="badges"></slot>
         </div>
-        <div class="content">
-          <div class="header">
-            ${this.trendIcon()}
-            <div class="percent">
-              ${percent.toFixed(0)}<span class="percent-symbol">%</span>
+        <button
+          class=${classMap({
+            wrapper: true,
+            [this.variant]: true,
+            [this.medium]: true,
+          })}
+          style="--percent: ${percent / 100}"
+        >
+          <div class="bar-container">
+            <div class="bar"></div>
+          </div>
+          <div class="content">
+            <div class="header">
+              ${this.trendIcon()}
+              <div class="percent">
+                ${percent.toFixed(0)}<span class="percent-symbol">%</span>
+              </div>
+            </div>
+            <div class="value">
+              <div class="current">
+                <slot name="current-value" class="current-value"
+                  >${this.value.toFixed(0)}</slot
+                ><span class="divider">/</span>
+              </div>
+              <div class="max">
+                <slot class="max-value" name="max-value"
+                  >${this.max.toFixed(0)}</slot
+                >
+                <slot class="unit" name="unit">m<sup>3</sup></slot>
+              </div>
             </div>
           </div>
-          <div class="value">
-            <div class="current">
-              <slot name="current-value" class="current-value"
-                >${this.value.toFixed(0)}</slot
-              ><span class="divider">/</span>
-            </div>
-            <div class="max">
-              <slot class="max-value" name="max-value"
-                >${this.max.toFixed(0)}</slot
-              >
-              <slot class="unit" name="unit">m<sup>3</sup></slot>
-            </div>
-          </div>
-        </div>
+        </button>
+        <div class="tag"><slot name="tag">${this.tag}</slot></div>
       </div>
     `;
   }
