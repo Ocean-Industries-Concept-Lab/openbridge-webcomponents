9ea349d4a4b5599825e17def82583711ab816774
Torstein A. Bø <torstein.bo@sintef.no>
2024-06-20 19:18:15 +0000

    6.0 double sided thruster

--- packages/openbridge-webcomponents/src/navigation-instruments/thruster/thruster.ts
+++ packages/openbridge-webcomponents/src/navigation-instruments/thruster/thruster.ts
@@ -1,6 +1,6 @@
-import {LitElement, svg, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import {Size, InstrumentState} from '../types';
+import { LitElement, svg, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { Size, InstrumentState } from '../types';
 
 /**
  * @element obc-thruster
@@ -11,29 +11,27 @@ import {Size, InstrumentState} from '../types';
  */
 @customElement('obc-thruster')
 export class ObcThruster extends LitElement {
-  @property({type: String}) size: Size = Size.medium;
-  @property({type: Number}) thrust: number = 0;
-  @property({type: Number}) setpoint: number | undefined;
-  @property({type: Boolean}) touching: boolean = false;
-  @property({type: Boolean}) atSetpoint: boolean = false;
-  @property({type: Boolean}) disableAutoAtSetpoint: boolean = false;
-  @property({type: Number}) autoAtSetpointDeadband: number = 1;
-  @property({type: Number}) setpointAtZeroDeadband: number = 0.5;
-  @property({type: String}) state: InstrumentState = InstrumentState.inCommand;
-  @property({type: Boolean}) tunnel: boolean = false;
-  @property({type: Boolean}) loading: boolean = false;
-  @property({type: Boolean}) off: boolean = false;
+  @property({ type: String }) size: Size = Size.medium;
+  @property({ type: Number }) thrust: number = 0;
+  @property({ type: Number }) setpoint: number | undefined;
+  @property({ type: Boolean }) touching: boolean = false;
+  @property({ type: Boolean }) atSetpoint: boolean = false;
+  @property({ type: Boolean }) disableAutoAtSetpoint: boolean = false;
+  @property({ type: Number }) autoAtSetpointDeadband: number = 1;
+  @property({ type: Number }) setpointAtZeroDeadband: number = 0.5;
+  @property({ type: String }) state: InstrumentState = InstrumentState.inCommand;
+  @property({ type: Boolean }) tunnel: boolean = false;
 
   override render() {
     return html`<div class="container">
       ${thruster(this.thrust, this.setpoint, this.state, {
-        atSetpoint: this.atSetpoint,
-        tunnel: this.tunnel,
-        setpointAtZeroDeadband: this.setpointAtZeroDeadband,
-        autoAtSetpoint: !this.disableAutoAtSetpoint,
-        autoSetpointDeadband: this.autoAtSetpointDeadband,
-        touching: this.touching,
-      })}
+      atSetpoint: this.atSetpoint,
+      tunnel: this.tunnel,
+      setpointAtZeroDeadband: this.setpointAtZeroDeadband,
+      autoAtSetpoint: !this.disableAutoAtSetpoint,
+      autoSetpointDeadband: this.autoAtSetpointDeadband,
+      touching: this.touching,
+    })}
     </div>`;
   }
 
@@ -53,43 +51,41 @@ const containerHeight = 134;
 
 function thrusterTop(
   value: number,
-  colors: {box: string; container: string},
+  colors: { box: string; container: string },
   hideTicks: boolean
 ) {
   const container = svg`
-      <path transform="translate(-33 -137)" d="M1 9C1 4.58172 4.58172 1 9 1H57C61.4183 1 65 4.58172 65 9V135H1V9Z" fill=${colors.container} stroke="var(--instrument-frame-tertiary-color)" vector-effect="non-scaling-stroke"/>
+      <path transform="translate(0 -2)" d="M -44 0  v -126  a 8 8 0 0 1 8 -8 h 72 a 8 8 0 0 1 8 8 V 0 Z" fill=${colors.container} stroke="var(--instrument-frame-tertiary-color)" vector-effect="non-scaling-stroke"/>
   `;
-  const track = svg`<rect width="32" height="134" x="-16" y="-136" fill="var(--instrument-frame-secondary-color)" stroke="var(--instrument-frame-tertiary-color)" vector-effect="non-scaling-stroke"/>`;
+  const track = svg`<rect width="40" height="134" x="-20" y="-136" fill="var(--instrument-frame-secondary-color)" stroke="var(--instrument-frame-tertiary-color)" vector-effect="non-scaling-stroke"/>`;
 
   const tickmarks = [];
 
-  const nTicks = 4;
+  const nTicks = 2;
   const delta = containerHeight / nTicks;
   if (!hideTicks) {
     for (let i = 1; i < nTicks; i++) {
       tickmarks.push(
-        svg`<line x1="-20" x2="-28" y1=${-i * delta - 2}  y2=${
-          -i * delta - 2
-        } stroke="var(--instrument-frame-tertiary-color)" stroke-width="1" vector-effect="non-scaling-stroke"/>`
+        svg`<line x1="-24" x2="-44" y1=${-i * delta - 2}  y2=${-i * delta - 2
+          } stroke="var(--instrument-frame-tertiary-color)" stroke-width="1" vector-effect="non-scaling-stroke"/>`
       );
       tickmarks.push(
-        svg`<line  x1="20"  x2="28" y1=${-i * delta - 2}  y2=${
-          -i * delta - 2
-        } stroke="var(--instrument-frame-tertiary-color)" stroke-width="1" vector-effect="non-scaling-stroke"/>`
+        svg`<line  x1="24"  x2="44" y1=${-i * delta - 2}  y2=${-i * delta - 2
+          } stroke="var(--instrument-frame-tertiary-color)" stroke-width="1" vector-effect="non-scaling-stroke"/>`
       );
     }
   }
 
   const barHeight = (134 * value) / 100;
   const barY = -136 + 134 - barHeight;
-  const bar = svg`<rect width="32" height=${barHeight} x="-16" y=${barY} fill=${colors.box} stroke=${colors.box} vector-effect="non-scaling-stroke"/>`;
+  const bar = svg`<rect width="40" height=${barHeight} x="-20" y=${barY} fill=${colors.box} stroke=${colors.box} vector-effect="non-scaling-stroke"/>`;
 
   return [container, track, tickmarks, bar];
 }
 
 function thrusterBottom(
   value: number,
-  colors: {box: string; container: string},
+  colors: { box: string; container: string },
   hideTicks: boolean
 ) {
   const container = svg`
@@ -108,20 +104,30 @@ function arrowTop(arrowColor: string) {
 function setpointSvg(
   value: number,
   setpointAtZero: boolean,
-  colors: {fill: string; stroke: string}
+  colors: { fill: string; stroke: string },
+  inCommand: boolean
 ) {
   const y =
-    -12 +
+    -14 +
     -(setpointAtZero
       ? 0
       : Math.sign(value) * ((containerHeight * Math.abs(value)) / 100 + 2));
-  return svg`
-  <g transform="translate(-40 ${y})">
-    <path d="M59.4001 11.1999C59.1483 11.3887 59 11.6852 59 12C59 12.3148 59.1483 12.6113 59.4001 12.8001L72.2001 22.3966C74.1773 23.879 77 22.4692 77 19.9971L77 4.0029C77 1.53076 74.1773 0.121035 72.2001 1.60338L59.4001 11.1999Z" fill=${colors.fill} stroke=${colors.stroke} stroke-width="2" stroke-linejoin="round" vector-effect="non-scaling-stroke"/>
-    <path d="M20.5999 12.8001C20.8517 12.6113 21 12.3148 21 12C21 11.6852 20.8517 11.3887 20.5999 11.1999L7.79986 1.60338C5.82268 0.121036 3 1.53075 3 4.0029L3 19.9971C3 22.4692 5.82268 23.879 7.79986 22.3966L20.5999 12.8001Z" fill=${colors.fill} stroke=${colors.stroke} stroke-width="2" stroke-linejoin="round" vector-effect="non-scaling-stroke"/>
-  </g>
+  if (inCommand) {
+    return svg`
+  <g transform="translate(-52 ${y})">
+    <path d="M79.4207 13.1845C79.1596 13.3724 79.0049 13.6744 79.0049 13.9961C79.0049 14.3178 79.1596 14.6198 79.4207 14.8077L95.4207 26.3235C97.668 27.941 101.005 26.4604 101.005 23.5926L101.005 4.39959C101.005 1.53179 97.668 0.0512117 95.4207 1.66865L79.4207 13.1845Z" fill=${colors.fill} stroke=${colors.stroke} stroke-width="2" stroke-linejoin="round" vector-effect="non-scaling-stroke"/>
+    <path d="M24.5793 14.8155C24.8404 14.6276 24.9951 14.3256 24.9951 14.0039C24.9951 13.6822 24.8404 13.3802 24.5793 13.1923L8.57928 1.67645C6.33203 0.059019 2.99512 1.5396 2.99512 4.40739L2.99512 23.6004C2.99512 26.4682 6.33203 27.9488 8.57928 26.3314L24.5793 14.8155Z" fill=${colors.fill} stroke=${colors.stroke} stroke-width="2" stroke-linejoin="round" vector-effect="non-scaling-stroke"/>
+  </g>`;
+  } else {
+    return svg`
+    <g transform="translate(-64 ${y})">
+<path d="M91.4158 13.1845C91.1548 13.3724 91 13.6744 91 13.9961C91 14.3178 91.1548 14.6198 91.4158 14.8077L107.416 26.3235C109.663 27.941 113 26.4604 113 23.5926L113 4.39959C113 1.5318 109.663 0.0512136 107.416 1.66865L91.4158 13.1845ZM107 18.6318L100.559 13.9961L107 9.36042L107 18.6318Z" fill=${colors.fill} stroke=${colors.stroke} stroke-width="2" stroke-linejoin="round" vector-effect="non-scaling-stroke"/>
+<path d="M36.5842 14.8155C36.8452 14.6276 37 14.3256 37 14.0039C37 13.6822 36.8452 13.3802 36.5842 13.1923L20.5842 1.67645C18.3369 0.0590192 15 1.5396 15 4.40739L15 23.6004C15 26.4682 18.3369 27.9488 20.5842 26.3314L36.5842 14.8155ZM21 9.36823L27.4408 14.0039L21 18.6396L21 9.36823Z" fill=${colors.fill} stroke=${colors.stroke} stroke-width="2" stroke-linejoin="round" vector-effect="non-scaling-stroke"/>
+</g>
 `;
+  }
 }
+
 
 export function thruster(
   thrust: number,
@@ -146,21 +152,21 @@ export function thruster(
   }
 
   let boxColor = 'var(--instrument-enhanced-secondary-color)';
-  let setPointColor = boxColor;
-  let arrowColor = 'var(--instrument-tick-mark-primary-color)';
+  let setPointColor = 'var(--instrument-enhanced-primary-color)';
+  let arrowColor = 'var(--instrument-regular-secondary-color)';
   let containerBackgroundColor = 'var(--instrument-frame-primary-color)';
   let zeroLineColor = 'var(--instrument-enhanced-secondary-color)';
   let hideTicks = false;
   if (options.atSetpoint) {
-    setPointColor = 'var(--instrument-frame-tertiary-color)';
+    setPointColor = boxColor;
   }
   if (state === InstrumentState.active) {
     boxColor = 'var(--instrument-regular-secondary-color)';
     zeroLineColor = 'var(--instrument-regular-secondary-color)';
-    setPointColor = boxColor;
+    setPointColor = 'var(--instrument-regular-primary-color)';
     arrowColor = 'var(--instrument-regular-secondary-color)';
     if (options.atSetpoint) {
-      setPointColor = 'var(--instrument-frame-tertiary-color)';
+      setPointColor = boxColor;
     }
   } else if (state === InstrumentState.loading) {
     boxColor = 'transparent';
@@ -186,7 +192,7 @@ export function thruster(
   }
 
   const centerLine = svg`
-    <rect x="-32" y="-2" width="64" height="4" fill=${zeroLineColor} stroke=${zeroLineColor}/>
+    <rect x="-44" y="-2" width="88" height="4" stroke-width="1" fill=${zeroLineColor} stroke=${zeroLineColor} vector-effect="non-scaling-stroke"/>
   `;
 
   const setpointAtZero =
@@ -195,12 +201,12 @@ export function thruster(
   const thrusterSvg = [
     thrusterTop(
       Math.max(thrust, 0),
-      {box: boxColor, container: containerBackgroundColor},
+      { box: boxColor, container: containerBackgroundColor },
       hideTicks
     ),
     thrusterBottom(
       Math.max(-thrust, 0),
-      {box: boxColor, container: containerBackgroundColor},
+      { box: boxColor, container: containerBackgroundColor },
       hideTicks
     ),
     centerLine,
@@ -209,8 +215,10 @@ export function thruster(
     thrusterSvg.push(
       setpointSvg(setpoint, setpointAtZero, {
         fill: setPointColor,
-        stroke: 'var(--border-silhouette-color)',
-      })
+        stroke: 'var(--border-silhouette-color)'
+      },
+        state === InstrumentState.inCommand,
+      )
     );
   }
 
