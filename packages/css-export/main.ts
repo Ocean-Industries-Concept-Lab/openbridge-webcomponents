5c4d1d86c2cedcab6513d11681698beb31a431ea
Torstein A. Bø <torsteinibo@gmail.com>
2024-10-11 14:54:04 +0200

    continue

--- packages/css-export/main.ts
+++ packages/css-export/main.ts
@@ -1,22 +1,43 @@
-import axios from "axios";
+import * as Figma from 'figma-js';
+
+const client = Figma.Client({
+  personalAccessToken: token
+});
 
 const baseUrl = "https://api.figma.com";
 const documentId = "8B6ypiC8UN7cqrLUXf7QuC";
 
-axios.defaults.headers.common["X-Figma-Token"] = token;
 
-const documentUrl = `${baseUrl}/v1/files/${documentId}`;
-const styleMetaUrl = `${baseUrl}/v1/files/${documentId}/styles`;
-const result = await axios.get(documentUrl);
-const element = result.data.document;
-const styleMetaResult = await axios.get(styleMetaUrl);
+async function getStyles(documentId: string) {
+  const result = await client.file(documentId)
+  const styles = result.data.styles;
+  const root = result.data.document;
 
-const styles: {key: string, node_id: string, name: string}[] = styleMetaResult.data.meta.styles;
+  for (const node_id in styles) {
+    const style = styles[node_id];
+    const node = await findNode(root, node_id);
+    console.log(style, node);
+  }
+}
 
-for (const s of styles) {
-  const k = s.node_id;
+async function findNode(node: Figma.Node, nodeId: string) {
+  if (node.id === nodeId) {
+    return node;
+  }
+
+
+  if ("children" in node && node.children) {
+    for (const child of node.children) {
+      const result = await findNode(child, nodeId);
+      if (result) {
+        return result;
+      }
+    }
+  }
+  return null;
 
 }
 
+getStyles(documentId);
 
 //console.log(result);
