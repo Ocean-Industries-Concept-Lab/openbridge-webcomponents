bfe646a188f1f58201c4bb24472c1a648a18a49d
Joachim <Joachim@hyke.no>
2024-11-04 13:43:22 +0000

    implement poi-target

--- packages/openbridge-webcomponents/src/navigation-instruments/poi-target/poi-target.stories.ts
+++ packages/openbridge-webcomponents/src/navigation-instruments/poi-target/poi-target.stories.ts
@@ -1,19 +1,39 @@
-import type { Meta, StoryObj } from '@storybook/web-components';
-import { ObcPoiTarget } from './poi-target';
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {ObcPoiTarget, Pointer} from './poi-target';
 import './poi-target';
+import {beta6Decorator} from '../../storybook-util';
 
 const meta: Meta<typeof ObcPoiTarget> = {
   title: 'Navigation Instruments/POI Target',
   tags: ['autodocs'],
-  component: "obc-poi-target",
+  component: 'obc-poi-target',
   args: {
+    height: 188,
+    value: 'checked',
+    pointerType: Pointer.Line,
   },
+  argTypes: {
+    height: {control: {type: 'range', min: 32, max: 243, step: 1}},
+    value: {
+      options: ['enabled', 'checked'],
+      control: {type: 'select'},
+    },
+    pointerType: {
+      options: [
+        Pointer.Line,
+        Pointer.ArrowLeft,
+        Pointer.ArrowRight,
+        Pointer.None,
+      ],
+      control: {type: 'select'},
+    },
+  },
+  decorators: [beta6Decorator],
 } satisfies Meta<ObcPoiTarget>;
 
 export default meta;
 type Story = StoryObj<ObcPoiTarget>;
 
 export const Primary: Story = {
-  args: {
-  },
-}
+  args: {},
+};
--- packages/openbridge-webcomponents/src/navigation-instruments/poi-target/poi-target.ts
+++ packages/openbridge-webcomponents/src/navigation-instruments/poi-target/poi-target.ts
@@ -1,22 +1,89 @@
-import { LitElement, html, unsafeCSS } from 'lit'
-import { customElement } from 'lit/decorators.js'
-import compentStyle from "./poi-target.css?inline";
+import {LitElement, html, unsafeCSS} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import compentStyle from './poi-target.css?inline';
+import {classMap} from 'lit/directives/class-map.js';
+import '../poi-line/poi-line';
+import '../../components/poi-target-button/poi-target-button';
+import {POIStyle} from '../poi-graphic-line/poi-config';
+import {pointerArrow} from './arrow';
+
+function valueToStyle(value: string): POIStyle {
+  let style = POIStyle.Normal;
+  switch (value) {
+    case 'enabled':
+      style = POIStyle.Normal;
+      break;
+    case 'checked':
+      style = POIStyle.Enhanced;
+      break;
+    default:
+      throw new Error(`Value has no style: ${style}`);
+  }
+  return style;
+}
+
+export enum Pointer {
+  Line = 'line',
+  ArrowLeft = 'arrow-left',
+  ArrowRight = 'arrow-right',
+  None = 'null',
+}
 
 @customElement('obc-poi-target')
 export class ObcPoiTarget extends LitElement {
+  @property({type: Number}) height: number = 188;
+  @property({type: String}) value: string = 'enabled';
+  @property({type: String}) pointerType: Pointer = Pointer.Line;
 
   override render() {
+    const style = valueToStyle(this.value);
+
+    let pointer = null;
+    let hasArrowPointer = false;
+    switch (this.pointerType) {
+      case Pointer.Line:
+        pointer = html`<obc-poi-line
+          height=${this.height}
+          lineStyle=${style}
+        ></obc-poi-line>`;
+        break;
+      case Pointer.ArrowLeft:
+        pointer = pointerArrow(this.pointerType, this.value);
+        hasArrowPointer = true;
+        break;
+      case Pointer.ArrowRight:
+        pointer = pointerArrow(this.pointerType, this.value);
+        hasArrowPointer = true;
+        break;
+      case Pointer.None:
+        pointer = null;
+        break;
+      default:
+        throw new Error(`Pointer type ${this.pointerType} not supported`);
+    }
+
     return html`
-      <div class="wrapper">
+      <div
+        class=${classMap({
+          wrapper: true,
+          ['type-' + this.pointerType]: true,
+          ['value-' + this.value]: true,
+        })}
+      >
+        <obc-poi-target-button
+          value=${this.value}
+          .hasPointer=${hasArrowPointer}
+        ></obc-poi-target-button>
+        ${pointer}
       </div>
-      `
+    `;
   }
 
-static override styles = unsafeCSS(compentStyle);
+  static override styles = unsafeCSS(compentStyle);
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'obc-poi-target': ObcPoiTarget
+    'obc-poi-target': ObcPoiTarget;
   }
 }
