5b39ac544b90f2d76661db2e82d5d694a91a7f73
Joachim <Joachim@hyke.no>
2024-11-04 13:40:25 +0000

    reflect changes in poi-config

--- packages/openbridge-webcomponents/src/navigation-instruments/poi-graphic-line/poi-graphic-line.stories.ts
+++ packages/openbridge-webcomponents/src/navigation-instruments/poi-graphic-line/poi-graphic-line.stories.ts
@@ -1,36 +1,28 @@
 import type {Meta, StoryObj} from '@storybook/web-components';
 import {ObcPoiGraphicLine} from './poi-graphic-line';
 import './poi-graphic-line';
-import {POIStyle, POIState} from './poi-config';
-import {beta6Decorator, widthDecorator} from '../../storybook-util';
+import {POIStyle} from './poi-config';
+import {beta6Decorator} from '../../storybook-util';
 
 const meta: Meta<typeof ObcPoiGraphicLine> = {
   title: 'Building blocks/POI Graphic Line',
   tags: ['autodocs'],
   component: 'obc-poi-graphic-line',
   argTypes: {
-    poiStyle: {
+    lineStyle: {
       options: Object.values(POIStyle),
       control: {
         type: 'radio',
       },
     },
-    poiState: {
-      options: Object.values(POIState),
-      control: {
-        type: 'radio',
-      },
-    },
 
-    width: {control: {type: 'range', min: 4, max: 48, step: 1}},
     height: {control: {type: 'range', min: 32, max: 192, step: 2}},
   },
   args: {
-    width: 4,
     height: 100,
-    poiStyle: POIStyle.enhanced,
-    poiState: POIState.solid,
+    lineStyle: POIStyle.Enhanced,
   },
+  decorators: [beta6Decorator],
 } satisfies Meta<ObcPoiGraphicLine>;
 
 export default meta;
