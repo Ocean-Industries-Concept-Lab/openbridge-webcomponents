3c5638de39a9a75f6d7c6177b81ae3fee461f16e
Torstein I. Bø <361267+tibnor@users.noreply.github.com>
2025-02-14 15:08:12 +0100

    feat: add label support to radio button component and update snapshots

--- packages/openbridge-webcomponents/src/components/radio/radio.stories.ts
+++ packages/openbridge-webcomponents/src/components/radio/radio.stories.ts
@@ -18,25 +18,23 @@ type Story = StoryObj<ObcRadio>;
 
 export const WithLabel: Story = {
   render: () => html`
-    <obc-radio hasLabel>Radio 1</obc-radio>`,
+    <obc-radio label="Radio 1"></obc-radio>`,
 }
 
 export const WithoutLabel: Story = {
-  args: {
-    hasLabel: false,
-  },
+
   render: () => html`<obc-radio></obc-radio>`,
 }
 
 export const CheckedWithLabel: Story = {
   render: () => html`
-    <obc-radio hasLabel checked>Radio 1</obc-radio>`,
+    <obc-radio label="Radio 1" checked></obc-radio>`,
 }
 
 export const Multiple: Story = {
   render: () => html`
-  <obc-radio hasLabel name="test" value="1">Radio 1</obc-radio>
-  <obc-radio hasLabel name="test" value="2">Radio 2</obc-radio>
-  <obc-radio hasLabel name="test" value="3">Radio 3</obc-radio>`,
+  <obc-radio label="Radio 1" name="test" value="1"></obc-radio>
+  <obc-radio label="Radio 2" name="test" value="2"></obc-radio>
+  <obc-radio label="Radio 3" name="test" value="3"></obc-radio>`,
 }
 
--- packages/openbridge-webcomponents/src/components/radio/radio.ts
+++ packages/openbridge-webcomponents/src/components/radio/radio.ts
@@ -1,40 +1,50 @@
-import { LitElement, html, unsafeCSS } from 'lit'
+import { LitElement, html } from 'lit'
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { customElement, property } from 'lit/decorators.js'
-import compentStyle from "./radio.css?inline";
 
+/**
+ * Radio button component
+ * 
+ * NOTE: This component does not use shadow DOM.
+ * This is because shadow DOM isolates the input from other radio buttons,
+ * which prevents the browser from managing the radio button group.
+ */
 @customElement('obc-radio')
 export class ObcRadio extends LitElement {
+  @property({ type: String }) label: string | undefined;
   @property({ type: String }) name: string | undefined;
   @property({ type: String }) value: string | undefined;
   @property({ type: Boolean }) checked: boolean = false;
   @property({ type: Boolean }) disabled: boolean = false;
   @property({ type: Boolean}) required: boolean = false;
-  @property({ type: Boolean}) hasLabel: boolean = false;
 
   override createRenderRoot() {
     return this; // Renders into light DOM
   }
 
+  onClick() {
+    this.renderRoot.querySelector('input')?.click();
+  }
+
   override render() {
-    if (this.hasLabel) {
+    if (this.label !== undefined) {
     return html`
-      <label for=${ifDefined(this.value)} class="has-label">
+      <label for=${ifDefined(this.value)} class="has-label obc-radio-button"
+        @click=${this.onClick}
+      >
         <input type="radio" .name=${ifDefined(this.name)} .value=${ifDefined(this.value)}
           ?checked=${this.checked} ?disabled=${this.disabled} ?required=${this.required}> 
-        <span class="label"><slot></slot></span>
+        <span class="label">${this.label}</span>
       </label>
       `
     } else {
       return html`
-      <input type="radio" .name=${ifDefined(this.name)} .value=${ifDefined(this.value)}
+      <input class="obc-radio-button" type="radio" .name=${ifDefined(this.name)} .value=${ifDefined(this.value)}
         ?checked=${this.checked} ?disabled=${this.disabled} ?required=${this.required}> 
       
       `
     }
   }
-
-static override styles = unsafeCSS(compentStyle);
 }
 
 declare global {
