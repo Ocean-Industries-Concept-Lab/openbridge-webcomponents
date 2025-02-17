c0035488c5f0759c2924d142535b6610dd490748
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-02-17 09:50:31 +0100

    lint

--- packages/openbridge-webcomponents/src/components/radio/radio.stories.ts
+++ packages/openbridge-webcomponents/src/components/radio/radio.stories.ts
@@ -1,36 +1,32 @@
-import type { Meta, StoryObj } from '@storybook/web-components';
-import { ObcRadio } from './radio';
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {ObcRadio} from './radio';
 import './radio';
-import { html } from 'lit';
+import {html} from 'lit';
 
 const meta: Meta<typeof ObcRadio> = {
   title: 'Input/Radio button',
   tags: ['autodocs', '6.0'],
-  component: "obc-radio",
+  component: 'obc-radio',
 } satisfies Meta<ObcRadio>;
 
 export default meta;
 type Story = StoryObj<ObcRadio>;
 
 export const WithLabel: Story = {
-  render: () => html`
-    <obc-radio label="Radio 1"></obc-radio>`,
-}
+  render: () => html` <obc-radio label="Radio 1"></obc-radio>`,
+};
 
 export const WithoutLabel: Story = {
-
   render: () => html`<obc-radio></obc-radio>`,
-}
+};
 
 export const CheckedWithLabel: Story = {
-  render: () => html`
-    <obc-radio label="Radio 1" checked></obc-radio>`,
-}
+  render: () => html` <obc-radio label="Radio 1" checked></obc-radio>`,
+};
 
 export const Multiple: Story = {
-  render: () => html`
-  <obc-radio label="Radio 1" name="test" value="1"></obc-radio>
-  <obc-radio label="Radio 2" name="test" value="2"></obc-radio>
-  <obc-radio label="Radio 3" name="test" value="3"></obc-radio>`,
-}
-
+  render: () =>
+    html` <obc-radio label="Radio 1" name="test" value="1"></obc-radio>
+      <obc-radio label="Radio 2" name="test" value="2"></obc-radio>
+      <obc-radio label="Radio 3" name="test" value="3"></obc-radio>`,
+};
--- packages/openbridge-webcomponents/src/components/radio/radio.ts
+++ packages/openbridge-webcomponents/src/components/radio/radio.ts
@@ -1,22 +1,22 @@
-import { LitElement, html } from 'lit'
-import { ifDefined } from 'lit/directives/if-defined.js';
-import { customElement, property } from 'lit/decorators.js'
+import {LitElement, html} from 'lit';
+import {ifDefined} from 'lit/directives/if-defined.js';
+import {customElement, property} from 'lit/decorators.js';
 
 /**
  * Radio button component
- * 
+ *
  * NOTE: This component does not use shadow DOM.
  * This is because shadow DOM isolates the input from other radio buttons,
  * which prevents the browser from managing the radio button group.
  */
 @customElement('obc-radio')
 export class ObcRadio extends LitElement {
-  @property({ type: String }) label: string | undefined;
-  @property({ type: String }) name: string | undefined;
-  @property({ type: String }) value: string | undefined;
-  @property({ type: Boolean }) checked: boolean = false;
-  @property({ type: Boolean }) disabled: boolean = false;
-  @property({ type: Boolean}) required: boolean = false;
+  @property({type: String}) label: string | undefined;
+  @property({type: String}) name: string | undefined;
+  @property({type: String}) value: string | undefined;
+  @property({type: Boolean}) checked: boolean = false;
+  @property({type: Boolean}) disabled: boolean = false;
+  @property({type: Boolean}) required: boolean = false;
 
   override createRenderRoot() {
     return this; // Renders into light DOM
@@ -28,27 +28,41 @@ export class ObcRadio extends LitElement {
 
   override render() {
     if (this.label !== undefined) {
-    return html`
-      <label for=${ifDefined(this.value)} class="has-label obc-radio-button"
-        @click=${this.onClick}
-      >
-        <input type="radio" .name=${ifDefined(this.name)} .value=${ifDefined(this.value)}
-          ?checked=${this.checked} ?disabled=${this.disabled} ?required=${this.required}> 
-        <span class="label">${this.label}</span>
-      </label>
-      `
+      return html`
+        <label
+          for=${ifDefined(this.value)}
+          class="has-label obc-radio-button"
+          @click=${this.onClick}
+        >
+          <input
+            type="radio"
+            .name=${ifDefined(this.name)}
+            .value=${ifDefined(this.value)}
+            ?checked=${this.checked}
+            ?disabled=${this.disabled}
+            ?required=${this.required}
+          />
+          <span class="label">${this.label}</span>
+        </label>
+      `;
     } else {
       return html`
-      <input class="obc-radio-button" type="radio" .name=${ifDefined(this.name)} .value=${ifDefined(this.value)}
-        ?checked=${this.checked} ?disabled=${this.disabled} ?required=${this.required}> 
-      
-      `
+        <input
+          class="obc-radio-button"
+          type="radio"
+          .name=${ifDefined(this.name)}
+          .value=${ifDefined(this.value)}
+          ?checked=${this.checked}
+          ?disabled=${this.disabled}
+          ?required=${this.required}
+        />
+      `;
     }
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'obc-radio': ObcRadio
+    'obc-radio': ObcRadio;
   }
 }
