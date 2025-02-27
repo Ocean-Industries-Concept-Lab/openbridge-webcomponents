f7f6e995dd9836b69003779fc80a5e6c1f5c37ff
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-02-27 21:00:39 +0100

    update: app button

--- packages/openbridge-webcomponents/src/components/app-button/app-button.ts
+++ packages/openbridge-webcomponents/src/components/app-button/app-button.ts
@@ -3,27 +3,32 @@ import {customElement, property} from 'lit/decorators.js';
 import compentStyle from './app-button.css?inline';
 import {classMap} from 'lit/directives/class-map.js';
 
+export enum AppButtonSize {
+  Normal = 'normal',
+  Small = 'small',
+}
+
 @customElement('obc-app-button')
 export class ObcAppButton extends LitElement {
   @property({type: String}) label = 'Button';
   @property({type: Boolean}) checked = false;
-  @property({type: String}) size = 'normal';
+  @property({type: String}) size = AppButtonSize.Normal;
 
   override render() {
-    return html` <div
+    return html` <button
       class="${classMap({
         wrapper: true,
         checked: this.checked,
-        small: this.size === 'small',
+        small: this.size === AppButtonSize.Small,
       })}"
     >
-      <button>
+      <div class="icon-wrapper">
         <span class="icon">
           <slot name="icon"></slot>
         </span>
-      </button>
-      <span class="label"> ${this.label} </span>
-    </div>`;
+      </div>
+      <div class="label"> ${this.label} </div>
+    </button>`;
   }
 
   static override styles = unsafeCSS(compentStyle);
