537109bc31eadda5df3885c74b8e2cb31d71c3b7
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-04-01 19:35:51 +0200

    feat[button]: add css part to wrapper and storybook example

--- packages/openbridge-webcomponents/src/components/button/button.stories.ts
+++ packages/openbridge-webcomponents/src/components/button/button.stories.ts
@@ -158,3 +158,24 @@ export const UncheckedDisabled: Story = {
     disabled: true,
   },
 };
+
+/** Use css parts to customize the button for special cases. Use this with caution */
+export const CssPart: Story = {
+  render: () => html`
+    <style>
+      obc-button::part(visible-wrapper) {
+        height: 100%;
+      }
+
+      obc-button::part(wrapper) {
+        height: 100%;
+      }
+
+      obc-button {
+        height: 200px;
+        display: block;
+      }
+    </style>
+    <obc-button variant="normal"> Test </obc-button>
+  `,
+};
