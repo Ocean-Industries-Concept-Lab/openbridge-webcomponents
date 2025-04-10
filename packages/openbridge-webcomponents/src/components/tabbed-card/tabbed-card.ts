8ae7df132efc0533c5a3c24a21d2548e41576753
Torstein A. Bø <torsteinibo@gmail.com>
2025-04-10 20:20:52 +0200

    fix

--- packages/openbridge-webcomponents/src/components/tabbed-card/tabbed-card.ts
+++ packages/openbridge-webcomponents/src/components/tabbed-card/tabbed-card.ts
@@ -1,5 +1,5 @@
 import {LitElement, html, unsafeCSS} from 'lit';
-import {customElement, property, state} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 import compentStyle from './tabbed-card.css?inline';
 
 export type ObcTabbedCardChangeEvent = CustomEvent<{
