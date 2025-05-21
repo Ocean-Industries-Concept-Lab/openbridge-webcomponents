f06379981d661936ceeb46c1447f2ff2750a431b
Torstein A. Bø <torstein@openbridgecomponents.no>
2025-05-21 15:57:43 +0200

    fix[rudder]: add instrument state and zero line (#144)

--- packages/openbridge-webcomponents/src/navigation-instruments/rudder/rudder.stories.ts
+++ packages/openbridge-webcomponents/src/navigation-instruments/rudder/rudder.stories.ts
@@ -2,7 +2,7 @@ import type {Meta, StoryObj} from '@storybook/web-components';
 import {ObcRudder} from './rudder.js';
 import './rudder.js';
 import {widthDecorator} from '../../storybook-util.js';
-
+import {InstrumentState} from '../types.js';
 const meta: Meta<typeof ObcRudder> = {
   title: 'Navigation Instruments/Rudder',
   tags: ['autodocs', '6.0'],
@@ -18,6 +18,7 @@ const meta: Meta<typeof ObcRudder> = {
     angle: {control: {type: 'range', min: -90, max: 90, step: 1}},
     maxAngle: {control: {type: 'range', min: 1, max: 90, step: 1}},
     setpoint: {control: {type: 'range', min: -90, max: 90, step: 1}},
+    state: {control: {type: 'select'}, options: Object.values(InstrumentState)},
   },
   decorators: [widthDecorator],
 } satisfies Meta<ObcRudder>;
