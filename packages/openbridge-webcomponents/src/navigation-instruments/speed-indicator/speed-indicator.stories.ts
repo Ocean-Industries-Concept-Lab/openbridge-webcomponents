a0a7884275c8030d663533edaa3fd22e3d7f7788
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-05-23 12:11:40 +0200

    feat: speed and rot indicators

---
+++ packages/openbridge-webcomponents/src/navigation-instruments/speed-indicator/speed-indicator.stories.ts
@@ -0,0 +1,35 @@
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {ObcSpeedIndicator} from './speed-indicator';
+import './speed-indicator';
+
+const meta: Meta<typeof ObcSpeedIndicator> = {
+  title: 'Navigation Instruments/Speed indicator',
+  tags: ['6.0'],
+  component: 'obc-speed-indicator',
+  argTypes: {
+    speed: {
+      control: {
+        type: 'range',
+        min: -20,
+        max: 100,
+        step: 1,
+      },
+    },
+    maxSpeed: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 1,
+      },
+    },
+  },
+  args: {},
+} satisfies Meta<ObcSpeedIndicator>;
+
+export default meta;
+type Story = StoryObj<ObcSpeedIndicator>;
+
+export const Primary: Story = {
+  args: {},
+};
