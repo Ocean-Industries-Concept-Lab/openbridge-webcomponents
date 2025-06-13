2d60327e8585b7f38e4aa807333b4bc6af165b27
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-06-13 13:28:19 +0200

    feat: abstract automation button
    Can be used for pump, motor, fan etc.
    
    Co-authored-by: ludvigovrevik <ludvigovrevik@users.noreply.github.com>

--- packages/openbridge-webcomponents/src/automation/pump/pump.ts
+++ packages/openbridge-webcomponents/src/automation/pump/pump.ts
@@ -1,42 +1,14 @@
-import {LitElement, html} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import {
-  AutomationBottonLabelStyle,
-  AutomationButtonDirection,
-  AutomationButtonDirectonValueLabel,
-  AutomationButtonLabelPosition,
-  AutomationButtonLabelSize,
-  AutomationButtonState,
-  AutomationButtonTagLabel,
-  AutomationButtonVariant,
-} from '../automation-button/automation-button.js';
+import {html} from 'lit';
+import {customElement} from 'lit/decorators.js';
 import '../../icons/icon-pump-off-horizontal.js';
 import '../../icons/icon-pump-on-horizontal.js';
 import '../../icons/icon-pump-off-vertical.js';
 import '../../icons/icon-pump-on-vertical.js';
 import '../automation-button/automation-button.js';
+import {ObcAbstractAutomationButton} from '../automation-button/abstract-automation-button.js';
 
 @customElement('obc-pump')
-export class ObcPump extends LitElement {
-  @property({type: String}) labelPosition: AutomationButtonLabelPosition =
-    AutomationButtonLabelPosition.bottom;
-  @property({type: String}) labelSize: AutomationButtonLabelSize =
-    AutomationButtonLabelSize.regular;
-  @property({type: String}) labelStyle: AutomationBottonLabelStyle =
-    AutomationBottonLabelStyle.regular;
-  @property({type: Boolean}) alert: boolean = false;
-  @property({type: Boolean}) progress: boolean = false;
-  @property({type: Boolean}) on: boolean = false;
-  @property({type: Number}) speedInPercent: number = 0;
-  @property({type: String}) tag: string = '';
-  @property({type: Boolean}) vertical: boolean = false;
-  @property({type: String}) variant: AutomationButtonVariant =
-    AutomationButtonVariant.regular;
-  @property({type: String}) direction: AutomationButtonDirection =
-    AutomationButtonDirection.forward;
-  @property({type: String}) labelDirection: 'up' | 'down' | 'left' | 'right' =
-    'right';
-
+export class ObcPump extends ObcAbstractAutomationButton {
   get icon() {
     if (this.vertical) {
       if (this.on) {
@@ -79,38 +51,6 @@ export class ObcPump extends LitElement {
           ></obi-pump-off-horizontal>`;
       }
     }
-  }
-
-  override render() {
-    const labels = [
-      {
-        type: 'direction',
-        value: this.speedInPercent,
-        nDigits: 3,
-        unit: 'percent',
-        direction: this.labelDirection,
-      } as AutomationButtonDirectonValueLabel,
-      {
-        type: 'tag',
-        text: this.tag,
-        showHash: true,
-      } as AutomationButtonTagLabel,
-    ];
-    return html`<obc-automation-button
-      .state=${this.on
-        ? AutomationButtonState.open
-        : AutomationButtonState.closed}
-      .labels=${labels}
-      .labelPosition=${this.labelPosition}
-      .labelSize=${this.labelSize}
-      .labelStyle=${this.labelStyle}
-      ?alert=${this.alert}
-      ?progress=${this.progress}
-      .variant=${this.variant}
-      .direction=${this.direction}
-    >
-      ${this.icon}
-    </obc-automation-button>`;
   }
 }
 
