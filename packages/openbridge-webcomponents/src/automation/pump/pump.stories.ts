e7c94aaa7a44b5e8d84a998ab3eb0a6c484914c1
ludvigovrevik <108417251+ludvigovrevik@users.noreply.github.com>
2025-06-13 13:46:58 +0200

    feat: implement pump component (#199)
    * feat: implement pump component
    
    Co-authored-by: Torstein A. Bø <tibnor@users.noreply.github.com>
    
    * added snapshots
    
    * fix
    
    ---------
    
    Co-authored-by: Torstein A. Bø <tibnor@users.noreply.github.com>
    Co-authored-by: Torstein A. Bø <361267+tibnor@users.noreply.github.com>

---
+++ packages/openbridge-webcomponents/src/automation/pump/pump.stories.ts
@@ -0,0 +1,70 @@
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {ObcPump} from './pump.js';
+import './pump.js';
+import {crossDecorator} from '../../storybook-util.js';
+import {
+  AutomationButtonDirection,
+  AutomationButtonVariant,
+} from '../automation-button/automation-button.js';
+
+const meta: Meta<typeof ObcPump> = {
+  title: 'Automation/Pump',
+  tags: ['6.0'],
+  component: 'obc-pump',
+  decorators: [crossDecorator],
+  args: {
+    tag: '007',
+  },
+} satisfies Meta<ObcPump>;
+
+export default meta;
+type Story = StoryObj<ObcPump>;
+
+export const OnVerical: Story = {
+  args: {
+    on: true,
+    vertical: true,
+    direction: AutomationButtonDirection.forward,
+    labelDirection: 'up',
+  },
+};
+
+export const OnHorizontal: Story = {
+  args: {
+    on: true,
+    vertical: false,
+    direction: AutomationButtonDirection.backwardFast,
+    labelDirection: 'left',
+  },
+};
+
+export const OffVertical: Story = {
+  args: {
+    on: false,
+    vertical: true,
+    direction: AutomationButtonDirection.standby,
+    labelDirection: 'up',
+  },
+};
+
+export const OffHorizontal: Story = {
+  args: {
+    on: false,
+    vertical: false,
+    direction: AutomationButtonDirection.backwardStopped,
+    labelDirection: 'right',
+  },
+};
+
+export const VariantDoubleSizeLarge: Story = {
+  args: {
+    on: true,
+    vertical: true,
+    direction: AutomationButtonDirection.forward,
+    labelDirection: 'up',
+    variant: AutomationButtonVariant.double,
+  },
+  globals: {
+    componentSize: 'obc-component-size-large',
+  },
+};
