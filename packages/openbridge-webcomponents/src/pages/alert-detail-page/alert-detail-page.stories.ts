30e01b1b622a52470dfdfb2e6c69860a44f109cb
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-06-25 21:07:43 +0200

    done

--- packages/openbridge-webcomponents/src/pages/alert-detail-page/alert-detail-page.stories.ts
+++ packages/openbridge-webcomponents/src/pages/alert-detail-page/alert-detail-page.stories.ts
@@ -1,33 +1,68 @@
-import type { Meta, StoryObj } from '@storybook/web-components-vite';
-import { ObcAlertDetailPage } from './alert-detail-page.js';
+import type {Meta, StoryObj} from '@storybook/web-components-vite';
+import {
+  AlertDetailPageAlertStatus,
+  AlertDetailPageType,
+  ObcAlertDetailPage,
+} from './alert-detail-page.js';
 import './alert-detail-page.js';
 import '../../components/alert-icon/alert-icon.js';
 import '../../icons/icon-alert-category-a.js';
-import { html } from 'lit';
+import '../../icons/icon-alarm-badge-outline.js';
+import {html} from 'lit';
+import {spread} from '@open-wc/lit-helpers';
 
 const meta: Meta<typeof ObcAlertDetailPage> = {
   title: 'Pages/Alert detail page',
   tags: ['6.0'],
-  component: "obc-alert-detail-page",
+  component: 'obc-alert-detail-page',
   args: {
-    canShelf: true,
-    canAck: true,
+    alertStatus: AlertDetailPageAlertStatus.active,
+    hasActions: true,
     hasReadoutGraph: true,
+    hasTagId: true,
+    hasCategory: true,
+    hasActivated: true,
+    hasTimer: true,
   },
-  parameters: {
-    layout: 'fullscreen',
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+      },
+      options: Object.values(AlertDetailPageType),
+    },
+    alertStatus: {
+      control: {
+        type: 'select',
+      },
+      options: Object.values(AlertDetailPageAlertStatus),
+    },
   },
   render: (args) => {
     return html`
-      <obc-alert-detail-page .canShelf=${args.canShelf} .canAck=${args.canAck} .hasReadoutGraph=${args.hasReadoutGraph}>
+      <obc-alert-detail-page
+        ${spread(args)}
+        style="height: ${args.type === AlertDetailPageType.page
+          ? 'calc(100vh - 2rem)'
+          : 'unset'}; display: block;"
+      >
         <obc-alert-icon slot="icon" name="alarm-unack"></obc-alert-icon>
         <span slot="title">Alert title</span>
-        <span slot="description">Description here. This alert has been triggered due to a dected anomaly.</span>
+        <span slot="description"
+          >Description here. This alert has been triggered due to a dected
+          anomaly.</span
+        >
         <span slot="tag-value">#000000</span>
-        <obi-icon-alert-category-a slot="category-value"></obi-icon-alert-category-a>
+        <obi-alert-category-a slot="category-value"></obi-alert-category-a>
         <span slot="activated-value">09:12:45</span>
         <span slot="timer-value">12s</span>
-    </obc-alert-detail-page>
+        <div
+          slot="readout-graph"
+          style="height: 200px; width: 100%; background: hsl(210, 100%, 90%);"
+        ></div>
+        <obc-button slot="action" fullWidth> Shelf </obc-button>
+        <obc-button slot="action" fullWidth variant="raised">ACK </obc-button>
+      </obc-alert-detail-page>
     `;
   },
 } satisfies Meta<ObcAlertDetailPage>;
@@ -35,7 +70,141 @@ const meta: Meta<typeof ObcAlertDetailPage> = {
 export default meta;
 type Story = StoryObj<ObcAlertDetailPage>;
 
-export const Primary: Story = {
+export const Page: Story = {
   args: {
+    type: AlertDetailPageType.page,
+    height: '100vh',
   },
-}
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Card: Story = {
+  args: {
+    type: AlertDetailPageType.card,
+  },
+};
+
+export const Modal: Story = {
+  args: {
+    type: AlertDetailPageType.modal,
+  },
+};
+
+export const Resolved: Story = {
+  args: {
+    type: AlertDetailPageType.modal,
+    alertStatus: AlertDetailPageAlertStatus.resolved,
+    hasAcknowledged: true,
+    hasAcknowledgedBy: true,
+    hasRectified: true,
+    hasTimer: undefined,
+    hasActions: undefined,
+  },
+  render: (args) => {
+    return html`
+      <obc-alert-detail-page
+        ${spread(args)}
+        style="height: ${args.height}; display: block;"
+      >
+        <obi-alarm-badge-outline slot="icon"></obi-alarm-badge-outline>
+        <span slot="title">Alert title</span>
+        <span slot="description"
+          >Description here. This alert has been triggered due to a dected
+          anomaly.</span
+        >
+        <span slot="tag-value">#000000</span>
+        <obi-alert-category-a slot="category-value"></obi-alert-category-a>
+        <span slot="activated-value">09:12:45</span>
+        <span slot="acknowledged-value">09:17:23</span>
+        <span slot="acknowledged-by-value">John Doe</span>
+        <span slot="rectified-value">09:18:00</span>
+        <div
+          slot="readout-graph"
+          style="height: 200px; width: 100%; background: hsl(210, 100%, 90%);"
+        ></div>
+      </obc-alert-detail-page>
+    `;
+  },
+};
+
+export const Shelved: Story = {
+  args: {
+    type: AlertDetailPageType.modal,
+    alertStatus: AlertDetailPageAlertStatus.shelved,
+    hasShelvedBy: true,
+    hasShelvingTimer: true,
+    hasTimer: undefined,
+  },
+  render: (args) => {
+    return html`
+      <obc-alert-detail-page
+        ${spread(args)}
+        style="height: ${args.height}; display: block;"
+      >
+        <obc-alert-icon slot="icon" name="alarm-unack"></obc-alert-icon>
+        <span slot="title">Alert title</span>
+        <span slot="description"
+          >Description here. This alert has been triggered due to a dected
+          anomaly.</span
+        >
+        <span slot="tag-value">#000000</span>
+        <obi-alert-category-a slot="category-value"></obi-alert-category-a>
+        <span slot="activated-value">09:12:45</span>
+        <span slot="shelving-timer-value">12s</span>
+        <span slot="shelved-by-value">John Doe</span>
+        <div
+          slot="readout-graph"
+          style="height: 200px; width: 100%; background: hsl(210, 100%, 90%);"
+        ></div>
+        <obc-button slot="action" fullWidth> Unshelve </obc-button>
+        <obc-button slot="action" fullWidth variant="raised">ACK </obc-button>
+      </obc-alert-detail-page>
+    `;
+  },
+};
+
+export const Blocked: Story = {
+  args: {
+    type: AlertDetailPageType.modal,
+    alertStatus: AlertDetailPageAlertStatus.blocked,
+    hasSubdescription: true,
+  },
+  render: (args) => {
+    return html`
+      <obc-alert-detail-page
+        ${spread(args)}
+        style="height: ${args.height}; display: block;"
+      >
+        <obi-alarm-badge-outline slot="icon"></obi-alarm-badge-outline>
+        <span slot="title">Alert title</span>
+        <span slot="description"
+          >Description here. This alert has been triggered due to a dected
+          anomaly.</span
+        >
+        <span slot="subdescription-label">Blocked alert note</span>
+        <span slot="subdescription"
+          >Some description or note that states why the alert is blocked.</span
+        >
+        <span slot="tag-value">#000000</span>
+        <obi-alert-category-a slot="category-value"></obi-alert-category-a>
+        <span slot="activated-value">09:12:45</span>
+        <span slot="timer-value">12s</span>
+        <div
+          slot="readout-graph"
+          style="height: 200px; width: 100%; background: hsl(210, 100%, 90%);"
+        ></div>
+        <obc-button slot="action" fullWidth> Review </obc-button>
+        <obc-button slot="action" fullWidth>Unblock </obc-button>
+      </obc-alert-detail-page>
+    `;
+  },
+};
+
+export const NoReadoutGraph: Story = {
+  args: {
+    type: AlertDetailPageType.modal,
+    hasReadoutGraph: undefined,
+  },
+};
--- packages/openbridge-webcomponents/src/pages/alert-detail-page/alert-detail-page.ts
+++ packages/openbridge-webcomponents/src/pages/alert-detail-page/alert-detail-page.ts
@@ -1,94 +1,168 @@
-import { LitElement, html, nothing, unsafeCSS } from 'lit'
-import { customElement } from '../../decorator.js'
-import compentStyle from "./alert-detail-page.css?inline";
-import { property } from 'lit/decorators.js';
+import {LitElement, html, nothing, unsafeCSS} from 'lit';
+import {customElement} from '../../decorator.js';
+import compentStyle from './alert-detail-page.css?inline';
+import {property} from 'lit/decorators.js';
 import '../../components/button/button.js';
-import { ButtonVariant } from '../../components/button/button.js';
+import '../../components/icon-button/icon-button.js';
+import '../../icons/icon-close-google.js';
+import '../../icons/icon-alerts-shelf.js';
+import '../../icons/icon-command-locked-f.js';
+
+export enum AlertDetailPageType {
+  page = 'page',
+  modal = 'modal',
+  card = 'card',
+}
+
+export enum AlertDetailPageAlertStatus {
+  active = 'active',
+  acknowledged = 'acknowledged',
+  shelved = 'shelved',
+  resolved = 'resolved',
+  blocked = 'blocked',
+}
 
 @customElement('obc-alert-detail-page')
 export class ObcAlertDetailPage extends LitElement {
-  @property({type: Boolean}) canShelf = false;
-  @property({type: Boolean}) canAck = false;
+  @property({type: String}) type: AlertDetailPageType =
+    AlertDetailPageType.page;
+  @property({type: String}) alertStatus: AlertDetailPageAlertStatus =
+    AlertDetailPageAlertStatus.active;
+
+  @property({type: Boolean}) hasSubdescription = false;
+  @property({type: Boolean}) hasActions = false;
+  @property({type: Boolean}) hasTagId = false;
+  @property({type: Boolean}) hasCategory = false;
+  @property({type: Boolean}) hasActivated = false;
+  @property({type: Boolean}) hasTimer = false;
+  @property({type: Boolean}) hasAcknowledged = false;
+  @property({type: Boolean}) hasAcknowledgedBy = false;
+  @property({type: Boolean}) hasRectified = false;
+  @property({type: Boolean}) hasShelvingTimer = false;
+  @property({type: Boolean}) hasShelvedBy = false;
   @property({type: Boolean}) hasReadoutGraph = false;
 
+  get alertStatusIcon() {
+    if (this.alertStatus === AlertDetailPageAlertStatus.shelved) {
+      return html`<obi-alerts-shelf class="icon status"></obi-alerts-shelf>`;
+    } else if (this.alertStatus === AlertDetailPageAlertStatus.blocked) {
+      return html`<obi-command-locked-f
+        class="icon status"
+      ></obi-command-locked-f>`;
+    }
+    return nothing;
+  }
+
+  private renderDetail(
+    has: boolean,
+    slotName: string,
+    defaultTitle: string,
+    isIcon: boolean = false
+  ) {
+    if (has) {
+      return html`
+        <div class="detail">
+          <div class="detail-title">
+            <slot name="${slotName}-label">${defaultTitle}</slot>
+          </div>
+          ${isIcon
+            ? html`<div class="detail-icon">
+                <slot name="${slotName}-value"></slot>
+              </div>`
+            : html`<div class="detail-value">
+                <slot name="${slotName}-value"></slot>
+              </div>`}
+        </div>
+      `;
+    }
+    return nothing;
+  }
+
   override render() {
+    const showCloseButton = [
+      AlertDetailPageType.modal,
+      AlertDetailPageType.page,
+    ].includes(this.type);
+
     return html`
-      <div class="wrapper">
+      <div class="wrapper type-${this.type} status-${this.alertStatus}">
         <div class="header">
-          <div class="icon">
+          ${this.alertStatusIcon}
+          <div class="icon alert">
             <slot name="icon"></slot>
           </div>
           <div class="title">
             <slot name="title"></slot>
           </div>
+          ${showCloseButton
+            ? html`<div class="close-button">
+                <obc-icon-button variant="flat">
+                  <obi-close-google></obi-close-google>
+                </obc-icon-button>
+              </div>`
+            : nothing}
         </div>
         <div class="divider"></div>
         <div class="body">
           <div class="description">
             <slot name="description"></slot>
           </div>
-          <div class="detail">
-            <div class="detail-title">
-              <slot name="tag-label">Tag ID</slot>
-            </div>
-            <div class="detail-value">
-              <slot name="tag-value"></slot>
-            </div>
-          </div>
-          <div class="detail">
-            <div class="detail-title">
-              <slot name="tag-label">Category</slot>
-            </div>
-            <div class="detail-value">
-              <slot name="category-value"></slot>
-            </div>
-          </div>
-          <div class="detail">
-            <div class="detail-title">
-              <slot name="tag-label">Activated</slot>
-            </div>
-            <div class="detail-value">
-              <slot name="activated-value"></slot>
-            </div>
-          </div>
-          <div class="detail">
-            <div class="detail-title">
-              <slot name="timer-label">Alert timer</slot>
-            </div>
-            <div class="detail-value">
-              <slot name="timer-value"></slot>
-            </div>
-          </div>
-          ${this.hasReadoutGraph ? html`
-            <div class="readout-graph">
-              <div class="readout-graph-title">
-                <slot name="readout-graph-title">Readout</slot>
-              </div>
-              <div class="readout-graph-container">
-                <slot name="readout-graph"></slot>
-              </div>
-            </div>
-          ` : nothing}
+          ${this.hasSubdescription
+            ? html`<div class="description sub-description">
+                <div class="label">
+                  <slot name="subdescription-label">Subdescription</slot>
+                </div>
+                <slot name="subdescription"></slot>
+              </div>`
+            : nothing}
+          ${this.renderDetail(this.hasTagId, 'tag', 'Tag ID')}
+          ${this.renderDetail(this.hasCategory, 'category', 'Category', true)}
+          ${this.renderDetail(this.hasActivated, 'activated', 'Activated')}
+          ${this.renderDetail(this.hasTimer, 'timer', 'Alert timer')}
+          ${this.renderDetail(
+            this.hasAcknowledged,
+            'acknowledged',
+            'Acknowledged'
+          )}
+          ${this.renderDetail(this.hasRectified, 'rectified', 'Rectified')}
+          ${this.renderDetail(
+            this.hasAcknowledgedBy,
+            'acknowledged-by',
+            'Acknowledged by'
+          )}
+          ${this.renderDetail(
+            this.hasShelvingTimer,
+            'shelving-timer',
+            'Shelfing timer'
+          )}
+          ${this.renderDetail(this.hasShelvedBy, 'shelved-by', 'Shelved by')}
+          ${this.hasReadoutGraph
+            ? html`
+                <div class="readout-graph">
+                  <div class="readout-graph-title">
+                    <slot name="readout-graph-title">Readout</slot>
+                  </div>
+                  <div class="readout-graph-container">
+                    <slot name="readout-graph"></slot>
+                  </div>
+                </div>
+              `
+            : nothing}
         </div>
-        <div class="actions">
-          ${this.canShelf ? html`
-            <obc-button .variant=${ButtonVariant.normal} fullWidth>
-              <slot name="shelf-label">Shelf</slot>
-            </obc-button>
-          ` : nothing}
-          <obc-button .variant=${ButtonVariant.raised} .disabled=${!this.canAck} fullWidth>
-            <slot name="ack-label">ACK</slot>
-          </obc-button>
-        </div>
+        ${this.hasActions
+          ? html`<div class="actions">
+              <slot name="action"></slot>
+            </div>`
+          : nothing}
       </div>
-      `
+    `;
   }
 
-static override styles = unsafeCSS(compentStyle);
+  static override styles = unsafeCSS(compentStyle);
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'obc-alert-detail-page': ObcAlertDetailPage
+    'obc-alert-detail-page': ObcAlertDetailPage;
   }
 }
