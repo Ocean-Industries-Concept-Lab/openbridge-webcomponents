7df602c6ff02dbc03deb8a8bb6cc55364ed52067
Torstein A. Bø <torstein@openbridgecomponents.no>
2025-09-10 20:21:52 +0200

    fix[rudder]: scaling safari (#344)

--- packages/openbridge-webcomponents/src/navigation-instruments/rudder/rudder.ts
+++ packages/openbridge-webcomponents/src/navigation-instruments/rudder/rudder.ts
@@ -209,19 +209,11 @@ export class ObcRudder extends LitElement {
       height: 100%;
     }
 
-    obc-watch {
+    .container > * {
       position: absolute;
       top: 0;
       left: 0;
-      bottom: 0;
-      height: 100%;
-    }
-
-    svg {
-      position: absolute;
-      top: 0;
-      left: 0;
-      max-width: 100%;
+      width: 100%;
       height: 100%;
     }
   `;
