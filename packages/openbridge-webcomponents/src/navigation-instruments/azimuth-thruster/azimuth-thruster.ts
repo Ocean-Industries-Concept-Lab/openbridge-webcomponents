8c83e16437cdd46872332f7d5a2212cce34b19c2
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-12-03 13:09:33 +0000

    fix

--- packages/openbridge-webcomponents/src/navigation-instruments/azimuth-thruster/azimuth-thruster.ts
+++ packages/openbridge-webcomponents/src/navigation-instruments/azimuth-thruster/azimuth-thruster.ts
@@ -173,35 +173,37 @@ export class ObcAzimuthThruster extends LitElement {
     }
 
     return html`
-      <obc-watch
-        .tickmarks=${tickmarks}
-        .state=${this.state}
-        .angleSetpoint=${this.angleSetpoint}
-        .atAngleSetpoint=${this.atAngleSetpointCalc}
-        .tickmarksInside=${this.tickmarksInside}
-        padding=${ifDefined(this.noPadding ? 16 : undefined)}
-        .advices=${this.angleAdviceRaw}
-        .starboardPortIndicator=${this.starboardPortIndicator}
-      ></obc-watch>
-      <svg viewBox=${viewBox} xmlns="http://www.w3.org/2000/svg">
-        <g transform="rotate(${rotateAngle})">
-          ${thruster(this.thrust, this.thrustSetpoint, this.state, {
-            atSetpoint: this.atThrustSetpoint,
-            singleSided: true,
-            singleDirection: false,
-            singleDirectionHalfSize: this.singleDirection,
-            tunnel: false,
-            autoAtSetpoint: !this.disableAutoAtThrustSetpoint,
-            autoSetpointDeadband: this.autoAtThrustSetpointDeadband,
-            setpointAtZeroDeadband: this.thrustSetpointAtZeroDeadband,
-            touching: this.touching,
-            advices: this.thrustAdvices,
-            topPropeller: this.topPropeller,
-            bottomPropeller: this.bottomPropeller,
-            narrow: true,
-          })}
-        </g>
-      </svg>
+      <div class="container">
+        <obc-watch
+          .tickmarks=${tickmarks}
+          .state=${this.state}
+          .angleSetpoint=${this.angleSetpoint}
+          .atAngleSetpoint=${this.atAngleSetpointCalc}
+          .tickmarksInside=${this.tickmarksInside}
+          padding=${ifDefined(this.noPadding ? 16 : undefined)}
+          .advices=${this.angleAdviceRaw}
+          .starboardPortIndicator=${this.starboardPortIndicator}
+        ></obc-watch>
+        <svg viewBox=${viewBox} xmlns="http://www.w3.org/2000/svg">
+          <g transform="rotate(${rotateAngle})">
+            ${thruster(this.thrust, this.thrustSetpoint, this.state, {
+              atSetpoint: this.atThrustSetpoint,
+              singleSided: true,
+              singleDirection: false,
+              singleDirectionHalfSize: this.singleDirection,
+              tunnel: false,
+              autoAtSetpoint: !this.disableAutoAtThrustSetpoint,
+              autoSetpointDeadband: this.autoAtThrustSetpointDeadband,
+              setpointAtZeroDeadband: this.thrustSetpointAtZeroDeadband,
+              touching: this.touching,
+              advices: this.thrustAdvices,
+              topPropeller: this.topPropeller,
+              bottomPropeller: this.bottomPropeller,
+              narrow: true,
+            })}
+          </g>
+        </svg>
+      </div>
     `;
   }
 
