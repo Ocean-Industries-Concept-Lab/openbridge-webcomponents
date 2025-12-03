6636bf858108f93007bdb52d3b0ab38f315f4437
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2025-12-03 09:46:41 +0000

    chore: update storybook to v10

--- packages/openbridge-webcomponents/.storybook/main.ts
+++ packages/openbridge-webcomponents/.storybook/main.ts
@@ -1,6 +1,10 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import {createRequire} from 'node:module';
 import {dirname, join} from 'path';
 import type {StorybookConfig} from '@storybook/web-components-vite';
 import FullReload from 'vite-plugin-full-reload';
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   stories: [
@@ -69,6 +73,6 @@ const config: StorybookConfig = {
 };
 export default config;
 
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, 'package.json')));
 }
