3c7d37e8341138d0d72dfea5dbbb1932281c9272
Thomas Søresnen <141747351+thomsoren@users.noreply.github.com>
2025-12-22 17:06:20 +0100

    Feat/refacto automation button (#407)
    * feat: refactor automation button to use readout stack component and remove deprecated label handling
    
    * fix: more props on readout
    
    * fix: add off, on and button
    
    * fix: add automation-label mixin
    
    * fix: size and idorientation as options
    
    * fix: remove hasValue
    
    * fix: adjust storybook to readoutlist
    
    * refactor: automation button with new readout-stack
    
    * refactor: stories to new readout-stack
    
    * fix: refactor and remove type error in automation stories
    
    * update snapshots
    
    * format
    
    * fix
    
    * fix
    
    * fix
    
    * fix:  label-empty class ignores tag when determining empty state
    
    * chore: ignore local Claude settings
    
    * fix: run lint fix
    
    * fix: resolve build and lint fail
    
    * chore(prettier): fix formatting
    
    * fix: add image for playwright test!
    
    * fix: add missing images
    
    * fix: add new image
    
    ---------
    
    Co-authored-by: Torstein A. Bø <361267+tibnor@users.noreply.github.com>

--- packages/openbridge-webcomponents/src/automation/digital-valve/digital-valve.ts
+++ packages/openbridge-webcomponents/src/automation/digital-valve/digital-valve.ts
@@ -1,12 +1,14 @@
 import {LitElement, html} from 'lit';
 import {property} from 'lit/decorators.js';
 import {
-  AutomationBottonLabelStyle,
-  AutomationButtonLabelPosition,
-  AutomationButtonLabelSize,
+  AutomationButtonReadoutPosition,
   AutomationButtonState,
-  AutomationButtonTagLabel,
 } from '../automation-button/automation-button.js';
+import {
+  AutomationButtonReadoutStack,
+  AutomationButtonReadoutStackSize,
+  AutomationButtonReadoutStackTag,
+} from '../../components/automation-button-readout-stack/automation-button-readout-stack.js';
 import '../../icons/icon-twoway-digital-open.js';
 import '../../icons/icon-twoway-digital-closed.js';
 import {customElement} from '../../decorator.js';
@@ -18,12 +20,10 @@ import {customElement} from '../../decorator.js';
  */
 @customElement('obc-digital-valve')
 export class ObcDigitalValve extends LitElement {
-  @property({type: String}) labelPosition: AutomationButtonLabelPosition =
-    AutomationButtonLabelPosition.bottom;
-  @property({type: String}) labelSize: AutomationButtonLabelSize =
-    AutomationButtonLabelSize.regular;
-  @property({type: String}) labelStyle: AutomationBottonLabelStyle =
-    AutomationBottonLabelStyle.regular;
+  @property({type: String}) readoutPosition: AutomationButtonReadoutPosition =
+    AutomationButtonReadoutPosition.bottom;
+  @property({type: String}) readoutSize: AutomationButtonReadoutStackSize =
+    AutomationButtonReadoutStackSize.regular;
   @property({type: Boolean}) alert: boolean = false;
   @property({type: Boolean}) progress: boolean = false;
   @property({type: Boolean}) open: boolean = false;
@@ -31,22 +31,19 @@ export class ObcDigitalValve extends LitElement {
   @property({type: Boolean}) vertical: boolean = false;
 
   override render() {
-    const labels = [
-      {
-        type: 'tag',
-        text: this.tag,
-        showHash: false,
-      } as AutomationButtonTagLabel,
-    ];
+    const readouts: AutomationButtonReadoutStack[] = [];
+    const tagValue: AutomationButtonReadoutStackTag | null = this.tag
+      ? {value: this.parseTagToNumber(this.tag)}
+      : null;
     const transform = this.vertical ? 'transform: rotate(90deg);' : '';
     return html`<obc-automation-button
       .state=${this.open
         ? AutomationButtonState.open
         : AutomationButtonState.closed}
-      .labels=${labels}
-      .labelPosition=${this.labelPosition}
-      .labelSize=${this.labelSize}
-      .labelStyle=${this.labelStyle}
+      .readouts=${readouts}
+      .tag=${tagValue}
+      .readoutPosition=${this.readoutPosition}
+      .readoutSize=${this.readoutSize}
       ?alert=${this.alert}
       ?progress=${this.progress}
     >
@@ -62,6 +59,11 @@ export class ObcDigitalValve extends LitElement {
             style="display: block; ${transform} line-height: 0;"
           ></obi-twoway-digital-closed>`}
     </obc-automation-button>`;
+  }
+
+  private parseTagToNumber(tag: string): number {
+    const num = parseInt(tag.replace(/#/g, ''), 10);
+    return isNaN(num) ? 0 : num;
   }
 }
 
--- packages/openbridge-webcomponents/src/automation/valve/valve.ts
+++ packages/openbridge-webcomponents/src/automation/valve/valve.ts
@@ -2,26 +2,26 @@ import {LitElement, css, html} from 'lit';
 import {property} from 'lit/decorators.js';
 import '../automation-button/automation-button.js';
 import {
-  AutomationBottonLabelStyle,
-  AutomationButtonDirectonValueLabel,
-  AutomationButtonLabelPosition,
-  AutomationButtonLabelSize,
+  AutomationButtonLabelDirection,
+  AutomationButtonReadoutPosition,
   AutomationButtonState,
-  AutomationButtonTagLabel,
   AutomationButtonVariant,
 } from '../automation-button/automation-button.js';
+import {
+  AutomationButtonReadoutStack,
+  AutomationButtonReadoutStackSize,
+  AutomationButtonReadoutStackTag,
+} from '../../components/automation-button-readout-stack/automation-button-readout-stack.js';
 import {Direction} from '../../types.js';
 import '../valve-analoge-two-way-icon/valve-analog-two-way-icon.js';
 import {customElement} from '../../decorator.js';
 
 @customElement('obc-valve')
 export class ObcValve extends LitElement {
-  @property({type: String}) labelPosition: AutomationButtonLabelPosition =
-    AutomationButtonLabelPosition.bottom;
-  @property({type: String}) labelSize: AutomationButtonLabelSize =
-    AutomationButtonLabelSize.regular;
-  @property({type: String}) labelStyle: AutomationBottonLabelStyle =
-    AutomationBottonLabelStyle.regular;
+  @property({type: String}) readoutPosition: AutomationButtonReadoutPosition =
+    AutomationButtonReadoutPosition.bottom;
+  @property({type: String}) readoutSize: AutomationButtonReadoutStackSize =
+    AutomationButtonReadoutStackSize.regular;
   @property({type: Boolean}) alert: boolean = false;
   @property({type: Boolean}) progress: boolean = false;
   @property({type: String}) tag: string = '';
@@ -32,40 +32,58 @@ export class ObcValve extends LitElement {
   @property({type: String}) variant: AutomationButtonVariant =
     AutomationButtonVariant.regular;
 
+  private mapDirectionToLabelDirection(
+    direction: Direction
+  ): AutomationButtonLabelDirection {
+    switch (direction) {
+      case Direction.Up:
+        return AutomationButtonLabelDirection.up;
+      case Direction.Down:
+        return AutomationButtonLabelDirection.down;
+      case Direction.Left:
+        return AutomationButtonLabelDirection.left;
+      case Direction.Right:
+        return AutomationButtonLabelDirection.right;
+    }
+  }
+
   override render() {
-    const labels = [
-      {
-        type: 'tag',
-        text: this.tag,
-        showHash: this.showDirectionLabel,
-      } as AutomationButtonTagLabel,
-      {
-        type: 'direction',
-        nDigits: 3,
+    const readouts: AutomationButtonReadoutStack[] = [];
+
+    if (this.showDirectionLabel) {
+      readouts.push({
+        type: 'value',
         value: this.value,
-        direction: this.direction,
-        unit: 'percent',
-      } as AutomationButtonDirectonValueLabel,
-    ];
+        nDigits: 3,
+        unit: '%',
+        direction: this.mapDirectionToLabelDirection(this.direction),
+        hasIcon: true,
+      });
+    }
+
+    const tagValue: AutomationButtonReadoutStackTag | null = this.tag
+      ? {value: this.parseTagToNumber(this.tag)}
+      : null;
 
     return html`
       <obc-automation-button
         .state=${this.closed
           ? AutomationButtonState.closed
           : AutomationButtonState.open}
-        .labels=${labels}
-        .labelPosition=${this.labelPosition}
-        .labelSize=${this.labelSize}
-        .labelStyle=${this.labelStyle}
+        .readouts=${readouts}
+        .tag=${tagValue}
+        .readoutPosition=${this.readoutPosition}
+        .readoutSize=${this.readoutSize}
         ?alert=${this.alert}
         ?progress=${this.progress}
         .variant=${this.variant}
       >
         <obc-valve-analog-two-way-icon
-          class=${['up', 'down'].includes(this.direction)
+          class=${[Direction.Up, Direction.Down].includes(this.direction)
             ? 'vertical'
             : 'horizontal'}
           slot="icon"
+          .value=${this.value}
           ?closed=${this.closed}
         ></obc-valve-analog-two-way-icon>
         <slot name="badge-top-right"></slot>
@@ -74,6 +92,11 @@ export class ObcValve extends LitElement {
         <slot name="badge-bottom-right"></slot>
       </obc-automation-button>
     `;
+  }
+
+  private parseTagToNumber(tag: string): number {
+    const num = parseInt(tag.replace(/#/g, ''), 10);
+    return isNaN(num) ? 0 : num;
   }
 
   static override styles = css`
