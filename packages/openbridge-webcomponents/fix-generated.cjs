9c698a8fe27a1fe7196047152734fe08e1ea3e66
Ulrik Jorgensen <ulrik.jorgensen@openstudio.no>
2026-02-05 15:14:27 +0100

    Commit last changes such that they are not lost. However, they are probably not needed since the wrapper should be fixed instead.

--- packages/openbridge-webcomponents/fix-generated.cjs
+++ packages/openbridge-webcomponents/fix-generated.cjs
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const packageJson = require('@ocean-industries-concept-lab/openbridge-webcomponents-react/package.json');
+const tsConfigFile = require('@ocean-industries-concept-lab/openbridge-webcomponents-react/tsconfig.json');
 
 function addRepositoryToPackageJsonVue() {
   const packageJson = require('../openbridge-webcomponents-vue/package.json');
@@ -56,6 +58,99 @@ function fixFilePathInPackageJsonReact() {
   );
 }
 
+function fixPackageJsonAngular() {
+  const packageJson = require('../openbridge-webcomponents-ng/package.json');
+  const ngPackageJson = require('../openbridge-webcomponents-ng/ng-package.json');
+  const tsconfiglibJson = require('../openbridge-webcomponents-ng/tsconfig.lib.json');
+  packageJson.files = packageJson.files.map((file) => {
+    return file.replace('dist/', '');
+  });
+  packageJson['scripts'] = {
+    'build': 'ng-packagr -p ng-package.json -c tsconfig.lib.json',
+  };
+  packageJson.dependencies['tslib'] = '^2.8.1';
+  packageJson.peerDependencies = {
+    '@angular/common': '^20.0.0',
+    '@angular/core': '^20.0.0',
+  };
+  packageJson['devDependencies'] = {
+    '@angular/core': '^20.0.1',
+    '@angular/common': '^20.0.1',
+    '@angular/compiler': '^20.0.1',
+    '@angular/compiler-cli': '^20.0.1',
+    'ng-packagr': '^20.0.1',
+  };
+
+  fs.writeFileSync(
+    '../openbridge-webcomponents-ng/package.json',
+    JSON.stringify(packageJson, null, 2)
+  );
+}
+
+function addTsConfigAngular() {
+  const tsconfig = {
+    compilerOptions: {
+      target: 'ES2022',
+      module: 'ES2022',
+      moduleResolution: 'Bundler',
+
+      strict: true,
+      skipLibCheck: true,
+
+      declaration: false,
+      sourceMap: true,
+
+      lib: ['ES2022', 'DOM'],
+      types: [],
+    },
+  };
+
+  fs.writeFileSync(
+    '../openbridge-webcomponents-ng/tsconfig.json',
+    JSON.stringify(tsconfig, null, 2) + '\n'
+  );
+}
+
+function addTsConfigLibAngular() {
+  const tsconfig = {
+    extends: './tsconfig.json',
+    compilerOptions: {
+      declaration: true,
+      declarationMap: true,
+      inlineSources: true,
+      target: 'ES2022',
+    },
+    angularCompilerOptions: {
+      compilationMode: 'partial',
+    },
+    exclude: ['**/*.spec.ts', 'dist', 'node_modules'],
+  };
+
+  fs.writeFileSync(
+    '../openbridge-webcomponents-ng/tsconfig.lib.json',
+    JSON.stringify(tsconfig, null, 2) + '\n'
+  );
+}
+
+function addNgPackageAngular() {
+  const content = {
+    $schema: './node_modules/ng-packagr/ng-package.schema.json',
+    dest: 'dist',
+    lib: {
+      entryFile: 'src/public-api.ts',
+    },
+  };
+
+  fs.writeFileSync(
+    '../openbridge-webcomponents-ng/ng-package.json',
+    JSON.stringify(content, null, 2) + '\n'
+  );
+}
+
 addRepositoryToPackageJsonVue();
 addRepositoryToPackageJsonReact();
 fixFilePathInPackageJsonReact();
+addNgPackageAngular();
+addTsConfigAngular();
+addTsConfigLibAngular();
+fixPackageJsonAngular();
