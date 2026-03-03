45e711f8f35ab713497fe31f839999dc3e6bc23b
Jon Dæhlen <jon.daehlen@openstudio.no>
2026-03-03 15:43:43 +0100

    Import obi-link

--- packages/openbridge-webcomponents/src/integration-systems/integration-bar/integration-bar.ts
+++ packages/openbridge-webcomponents/src/integration-systems/integration-bar/integration-bar.ts
@@ -14,6 +14,7 @@ import '../../icons/icon-home.js';
 import '../../icons/icon-ship.js';
 import '../../icons/icon-screen-desk.js';
 import '../../icons/icon-alerts.js';
+import '../../icons/icon-link.js';
 import {property} from 'lit/decorators.js';
 
 export enum IntegrationBarType {
