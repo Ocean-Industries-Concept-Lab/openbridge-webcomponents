1ff9a7ba3469e7f2c2972204052cf022e2c0c956
Torstein A. Bø <torstein@openbridgecomponents.no>
2026-03-27 08:12:57 +0100

    fix(toggle-switch): internal switch is not synced (#715)
    * fix(toggle-switch): internal switch is not synced

--- packages/openbridge-webcomponents/vitest.browser.config.ts
+++ packages/openbridge-webcomponents/vitest.browser.config.ts
@@ -3,6 +3,8 @@ import {playwright} from '@vitest/browser-playwright';
 
 export default defineConfig({
   test: {
+    // ignore files in dist folder
+    exclude: ['dist/**'],
     browser: {
       enabled: true,
       provider: playwright(),
