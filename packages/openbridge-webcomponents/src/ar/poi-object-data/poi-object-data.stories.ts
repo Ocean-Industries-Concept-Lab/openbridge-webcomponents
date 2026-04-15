6360d4f324d7cb6cd95adc6df575200eed1dfa40
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2026-04-15 06:58:39 +0000

    fix

--- packages/openbridge-webcomponents/src/ar/poi-object-data/poi-object-data.stories.ts
+++ packages/openbridge-webcomponents/src/ar/poi-object-data/poi-object-data.stories.ts
@@ -11,7 +11,7 @@ import '../../icons/icon-vessel-generic-default-filled.js';
 const meta: Meta = {
   title: 'AR/POI Button/POI Object Data',
   component: 'obc-poi-object-data',
-  tags: ['skip-test', 'autodocs', 'skip-test'],
+  tags: ['skip-test', 'autodocs'],
   argTypes: {
     type: {
       control: 'select',
