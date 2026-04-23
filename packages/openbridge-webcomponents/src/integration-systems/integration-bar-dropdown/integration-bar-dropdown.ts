9bec82c5036aaa7f36fa1067067b5131472fd469
Jon Dæhlen <jon.daehlen@openstudio.no>
2026-04-23 10:20:26 +0200

    Revert all files related to integration-dropdown-menu

--- packages/openbridge-webcomponents/src/integration-systems/integration-bar-dropdown/integration-bar-dropdown.ts
+++ packages/openbridge-webcomponents/src/integration-systems/integration-bar-dropdown/integration-bar-dropdown.ts
@@ -2,250 +2,141 @@ import {LitElement, html, nothing, unsafeCSS} from 'lit';
 import {customElement} from '../../decorator.js';
 import compentStyle from './integration-bar-dropdown.css?inline';
 import '../integration-tabs/integration-tabs.js';
-import '../integration-button/integration-button.js';
-import {IntegrationButtonVariant} from '../integration-button/integration-button.js';
 import '../../components/clock/clock.js';
 import '../../components/icon-button/icon-button.js';
 import '../../icons/icon-palette-day-night-iec.js';
 import '../../icons/icon-user.js';
 import '../../icons/icon-configure.js';
 import '../../icons/icon-notification.js';
+import '../../icons/icon-home.js';
+import {NotificationButtonStyle} from '../../components/notification-button/notification-button.js';
 import {property} from 'lit/decorators.js';
-import {classMap} from 'lit/directives/class-map.js';
-
-export enum IntegrationBarType {
-  vesselname = 'vessel-name',
-  rich = 'rich',
-  dropdown = 'dropdown',
-  inactive = 'inactive',
-  monitoring = 'monitoring',
-  novesselselection = 'no-vessel-selection',
-}
 
 /**
  *
- * @fires fleet-button-click - Fired when the fleet button is clicked
+ * @fires home-button-clicked - Fired when the home button is clicked
  * @fires dimming-button-clicked - Fired when the dimming button is clicked
+ * @fires notification-button-clicked - Firaed when the notification button is clicked
  * @fires user-button-clicked - Fired when the user button is clicked
  * @fires system-button-clicked - Fired when the system button is clicked
+ *
+ * @slot vessel-selector
+ * @slot status-label-1
+ * @slot status-icon-1
+ * @slot status-label-2
+ * @slot status-icon-2
+ * @slot status-label-3
+ * @slot status-icon-3
+ * @slot clock
  */
-@customElement('obc-integration-bar')
-export class ObcIntegrationBar extends LitElement {
+@customElement('obc-integration-bar-dropdown')
+export class ObcIntegrationBarDropdown extends LitElement {
+  @property({type: Boolean}) showHomeButton = false;
+  @property({type: Boolean}) homeButtonActivated = false;
   @property({type: Boolean}) showClock = false;
   @property({type: Boolean}) showNotificationButton = false;
   @property({type: Boolean}) notificationButtonActivated = false;
+  @property({type: Number}) notificationCount = 0;
+  @property({type: Boolean}) showNotificationCount = false;
   @property({type: Boolean}) showUserButton = false;
   @property({type: Boolean}) userButtonActivated = false;
   @property({type: Boolean}) showDimmingButton = false;
   @property({type: Boolean}) dimmingButtonActivated = false;
   @property({type: Boolean}) showSystemButton = false;
   @property({type: Boolean}) systemButtonActivated = false;
-  @property({type: Boolean}) fleetButtonSelected = false;
+  @property({type: Number}) nStatusFields = 0;
+
+  private renderStatusFields() {
+    if (this.nStatusFields <= 0) {
+      return nothing;
+    }
+    const result = [];
+    for (let i = 0; i < this.nStatusFields; i++) {
+      if (i > 0) {
+        result.push(html`<div class="divider"></div>`);
+      }
+      result.push(html`
+        <div class="status-item">
+          <slot class="status-icon" name="status-icon-${i + 1}"></slot>
+          <slot class="status-label" name="status-label-${i + 1}"></slot>
+        </div>
+      `);
+    }
+    return html`<div class="status">${result}</div>`;
+  }
 
   override render() {
     return html`
       <div class="wrapper">
         <div class="left-side">
-          <obc-integration-tabs
-            class="fleet-btn"
-            .selected=${this.fleetButtonSelected}
-            @click=${() =>
-              this.dispatchEvent(new CustomEvent('fleet-button-click'))}
-            >Fleet</obc-integration-tabs
-          >
+          ${this.showHomeButton
+            ? html`<obc-icon-button
+                class="home-button"
+                part="home-button"
+                variant="integration"
+                @click=${() =>
+                  this.dispatchEvent(new CustomEvent('home-button-clicked'))}
+                ?activated=${this.homeButtonActivated}
+              >
+                <obi-home></obi-home>
+              </obc-icon-button>`
+            : null}
           <slot name="vessel-selector"></slot>
+          ${this.renderStatusFields()}
         </div>
-        <div class="right-content-container">
-          ${
-            this.showAlertButton
-              ? html`<obc-icon-button
-                  class=${classMap({
-                    'alert-button': true,
-                    activated: this.alertButtonActivated,
-                  })}
-                  part="alert-button"
-                  variant="integration"
-                  style=${this.alertButtonActivated
-                    ? 'anchor-name: --settings-menu-anchor;'
-                    : ''}
-                  @click=${() =>
-                    this.dispatchEvent(new CustomEvent('alert-button-clicked'))}
-                  ?activated=${this.alertButtonActivated}
-                >
-                  <obi-alerts></obi-alerts>
-                </obc-icon-button>`
-              : null
-          }
-          ${
-            this.showNotificationButton
-              ? html`<obc-icon-button
-                  class="notification-button"
-                  part="notification-button"
-                  variant="integration"
-                  @click=${() =>
-                    this.dispatchEvent(
-                      new CustomEvent('notification-button-clicked')
-                    )}
-                  ?activated=${this.notificationButtonActivated}
-                >
-                  <obi-notification></obi-notification>
-                </obc-icon-button>`
-              : null
-          }
-          ${
-            this.showSystemButton
-              ? html`<obc-icon-button
-                  class=${classMap({
-                    'system-button': true,
-                    activated: this.systemButtonActivated,
-                  })}
-                  part="system-button"
-                  variant="integration"
-                  style=${this.systemButtonActivated
-                    ? 'anchor-name: --settings-menu-anchor;'
-                    : ''}
-                  @click=${() =>
-                    this.dispatchEvent(
-                      new CustomEvent('system-button-clicked')
-                    )}
-                  ?activated=${this.systemButtonActivated}
-                >
-                  <obi-configure></obi-configure>
-                </obc-icon-button>`
-              : null
-          }
-          ${
-            this.showDimmingButton
-              ? html`<obc-icon-button
-                  class=${classMap({
-                    'dimming-button': true,
-                    activated: this.dimmingButtonActivated,
-                  })}
-                  part="dimming-button"
-                  variant="integration"
-                  style=${this.dimmingButtonActivated
-                    ? 'anchor-name: --settings-menu-anchor;'
-                    : ''}
-                  @click=${() =>
-                    this.dispatchEvent(
-                      new CustomEvent('dimming-button-clicked')
-                    )}
-                  ?activated=${this.dimmingButtonActivated}
-                >
-                  <obi-palette-day-night-iec></obi-palette-day-night-iec>
-                </obc-icon-button>`
-              : null
-          }
-          ${
-            this.showUserButton
-              ? html`<obc-icon-button
-                  class=${classMap({
-                    'user-button': true,
-                    activated: this.userButtonActivated,
-                  })}
-                  part="user-button"
-                  variant="integration"
-                  style=${this.userButtonActivated
-                    ? 'anchor-name: --settings-menu-anchor;'
-                    : ''}
-                  @click=${() =>
-                    this.dispatchEvent(new CustomEvent('user-button-clicked'))}
-                  ?activated=${this.userButtonActivated}
-                >
-                  <obi-user></obi-user>
-                </obc-icon-button>`
-              : null
-          }
+        <div class="right-side">
+          ${this.showNotificationButton
+            ? html`<obc-notification-button
+                @click=${() =>
+                  this.dispatchEvent(
+                    new CustomEvent('notification-button-clicked')
+                  )}
+                .buttonStyle=${NotificationButtonStyle.Enhanced}
+                .showCount=${this.showNotificationCount}
+                .count=${this.notificationCount}
+                ?isActive=${this.notificationButtonActivated}
+              ></obc-notification-button>`
+            : nothing}
+          ${this.showSystemButton
+            ? html`<obc-icon-button
+                class="system-button"
+                part="system-button"
+                variant="integration"
+                @click=${() =>
+                  this.dispatchEvent(new CustomEvent('system-button-clicked'))}
+                ?activated=${this.systemButtonActivated}
+              >
+                <obi-configure></obi-configure>
+              </obc-icon-button>`
+            : null}
+          ${this.showDimmingButton
+            ? html`<obc-icon-button
+                class="dimming-button"
+                part="dimming-button"
+                variant="integration"
+                @click=${() =>
+                  this.dispatchEvent(new CustomEvent('dimming-button-clicked'))}
+                ?activated=${this.dimmingButtonActivated}
+              >
+                <obi-palette-day-night-iec></obi-palette-day-night-iec>
+              </obc-icon-button>`
+            : null}
+          ${this.showUserButton
+            ? html`<obc-icon-button
+                class="user-button"
+                part="user-button"
+                variant="integration"
+                @click=${() =>
+                  this.dispatchEvent(new CustomEvent('user-button-clicked'))}
+                ?activated=${this.userButtonActivated}
+              >
+                <obi-user></obi-user>
+              </obc-icon-button>`
+            : null}
           ${this.showClock ? html`<slot name="clock"></slot>` : null}
         </div>
-      </nav>
-      <div class="vessel-integration-menu-container">
-        <slot name="vessel-integration-menu"></slot>
-      </div>
-      <div class="right-side-system-menu-container">
-        <slot name="vessel-integration-menu"></slot>
       </div>
     `;
-  }
-
-  private renderFleetVesselContainerByType() {
-    switch (this.type) {
-      case IntegrationBarType.vesselname:
-        return this.renderVesselNameContent();
-      case IntegrationBarType.rich:
-        return this.renderRichContent();
-      case IntegrationBarType.dropdown:
-      case IntegrationBarType.inactive:
-      case IntegrationBarType.monitoring:
-      case IntegrationBarType.novesselselection:
-        return this.renderSelectorContent();
-      default:
-        return null;
-    }
-  }
-
-  private renderVesselNameContent() {
-    const fallbackVessels = [{value: 'default-vessel', label: 'Vessel name'}];
-    const vesselItems =
-      this.vesselSelectorOptions.length > 0
-        ? this.vesselSelectorOptions
-        : fallbackVessels;
-    const isFleetButtonAnchored = this.fleetButtonActivated;
-
-    return html`
-      <obc-integration-button
-        class="fleet-button"
-        .variant=${IntegrationButtonVariant.normal}
-        ?selected=${this.fleetButtonSelected}
-        ?activated=${this.fleetButtonActivated}
-        style=${isFleetButtonAnchored
-          ? 'anchor-name: --integration-menu-anchor;'
-          : ''}
-        @click=${() => this.onFleetButtonClick()}
-      >
-        <span slot="label">${this.fleetButtonLabel}</span>
-      </obc-integration-button>
-
-      <div class="vessel-container">
-        ${vesselItems.map((vessel, index) => {
-          const isSelected =
-            this.selectedVesselValue !== '' &&
-            this.selectedVesselValue === vessel.value;
-          const isActivated =
-            this.activeVesselValue !== '' &&
-            this.activeVesselValue === vessel.value;
-          const shouldRenderSeparator = index < vesselItems.length - 1;
-          return html`
-            <obc-integration-button
-              hasLeadingIcon
-              .variant=${isSelected
-                ? IntegrationButtonVariant.normal
-                : IntegrationButtonVariant.flat}
-              ?selected=${isSelected}
-              ?activated=${isActivated}
-              style=${isActivated
-                ? 'anchor-name: --integration-menu-anchor;'
-                : ''}
-              @click=${() => this.onVesselButtonClick(vessel)}
-            >
-              <obi-ship slot="leading-icon"></obi-ship>
-              <span slot="label">${vessel.label}</span>
-            </obc-integration-button>
-            ${shouldRenderSeparator
-              ? html`<div class="separator"></div>`
-              : null}
-          `;
-        })}
-      </div>
-    `;
-  }
-
-  private renderRichContent() {
-    return html``;
-  }
-
-  private renderSelectorContent() {
-    return html``;
   }
 
   static override styles = unsafeCSS(compentStyle);
--- packages/openbridge-webcomponents/src/integration-systems/integration-button/integration-button.stories.ts
+++ packages/openbridge-webcomponents/src/integration-systems/integration-button/integration-button.stories.ts
@@ -1,15 +1,10 @@
 import type {Meta, StoryObj} from '@storybook/web-components-vite';
-import {
-  IntegrationButtonType,
-  IntegrationButtonVariant,
-  ObcIntegrationButton,
-} from './integration-button.js';
+import {ObcIntegrationButton} from './integration-button.js';
 import './integration-button.js';
 import {html} from 'lit';
 import '../../icons/icon-placeholder.js';
-import {ButtonVariant} from '../../components/button/button.js';
 
-const meta: Meta<ObcIntegrationButton> = {
+const meta: Meta<typeof ObcIntegrationButton> = {
   title: 'Integration Systems/Integration Button',
   tags: ['6.0'],
   component: 'obc-integration-button',
@@ -21,10 +16,7 @@ const meta: Meta<ObcIntegrationButton> = {
       {label: 'Readout 1', value: 'Value 1', unit: 'Unit 1'},
       {label: 'Readout 2', value: 'Value 2', unit: 'Unit 2'},
     ],
-    variant: IntegrationButtonVariant.normal,
-    type: IntegrationButtonType.regular,
   },
-
   globals: {
     backgrounds: {
       value: 'integration-container-global-color',
@@ -39,8 +31,6 @@ const meta: Meta<ObcIntegrationButton> = {
       .readouts=${args.readouts}
       .selected=${args.selected}
       .disabled=${args.disabled}
-      .variant=${args.variant}
-      .type=${args.type}
     >
       <obi-placeholder slot="leading-icon"></obi-placeholder>
       <obi-placeholder slot="trailing-icon"></obi-placeholder>
@@ -54,58 +44,10 @@ const meta: Meta<ObcIntegrationButton> = {
 } satisfies Meta<ObcIntegrationButton>;
 export default meta;
 
-export const Primary: StoryObj<ObcIntegrationButton> = {};
+export const Primary: StoryObj<typeof ObcIntegrationButton> = {};
 
-export const WithDescription: StoryObj<ObcIntegrationButton> = {
-  args: {
-    hasDescription: true,
-  },
-  render: (args) => html`
-    <obc-integration-button
-      style="width: 320px; display: block;"
-      .hasLeadingIcon=${args.hasLeadingIcon}
-      .hasTrailingIcon=${args.hasTrailingIcon}
-      .hasTrailingIcon2=${args.hasTrailingIcon2}
-      .readouts=${args.readouts}
-      .selected=${args.selected}
-      .disabled=${args.disabled}
-      .variant=${args.variant}
-      .type=${args.type}
-      .hasDescription=${args.hasDescription}
-    >
-      <obi-placeholder slot="leading-icon"></obi-placeholder>
-      <obi-placeholder slot="trailing-icon"></obi-placeholder>
-      <obi-placeholder slot="trailing-icon2"></obi-placeholder>
-      <div slot="label">Label</div>
-      <div slot="status">A description</div>
-      <div slot="info-label">Info Label</div>
-      <div slot="info-status">Info Status</div>
-    </obc-integration-button>
-  `,
-};
-
-export const Selected: StoryObj<ObcIntegrationButton> = {
+export const Selected: StoryObj<typeof ObcIntegrationButton> = {
   args: {
     selected: true,
-  },
-};
-
-export const Flat: StoryObj<ObcIntegrationButton> = {
-  args: {
-    variant: IntegrationButtonVariant.flat,
-    hasTrailingIcon: false,
-  },
-};
-
-export const Hug: StoryObj<ObcIntegrationButton> = {
-  args: {
-    type: IntegrationButtonType.hug,
-    hasTrailingIcon: false,
-  },
-};
-
-export const Rich: StoryObj<ObcIntegrationButton> = {
-  args: {
-    type: IntegrationButtonType.rich,
   },
 };
