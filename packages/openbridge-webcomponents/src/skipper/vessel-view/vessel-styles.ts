ab70d2b90237af658815152715515088e304bf86
JOTRON\ext_husejn.alibasic <ext_husejn.alibasic@jotron.com>
2026-06-11 15:37:33 +0200

    finished side and top down view

---
+++ packages/openbridge-webcomponents/src/skipper/vessel-view/vessel-styles.ts
@@ -0,0 +1,63 @@
+import {css} from 'lit';
+export const vesselStyles = css`
+  .container {
+    height: 100%;
+    width: 100%;
+  }
+
+  .container > svg {
+    height: 100%;
+    width: 100%;
+  }
+
+  .satellite-label {
+    fill: var(--element-active-inverted-color);
+    font-size: 16px;
+    font-family: sans-serif;
+    text-anchor: middle;
+    dominant-baseline: middle;
+    pointer-events: none;
+  }
+
+  .ring {
+    fill: var(--instrument-enhanced-secondary-color);
+  }
+
+  .ring-dark {
+    fill-opacity: 0.75;
+  }
+
+  .ring-mid {
+    fill-opacity: 0.55;
+  }
+
+  .ring-light {
+    fill-opacity: 0.35;
+  }
+
+  .circle-dashed {
+    fill: none;
+    stroke: var(--instrument-frame-primary-color);
+    stroke-width: 1.2;
+    stroke-dasharray: 6 6;
+  }
+
+  .circle-solid {
+    fill: none;
+    stroke: var(--element-active-color);
+    stroke-width: 2;
+  }
+
+  .axis {
+    stroke: var(--instrument-frame-primary-color);
+    stroke-width: 1.4;
+  }
+
+  .label {
+    fill: var(--element-active-color);
+    font-size: 14px;
+    font-family: sans-serif;
+    text-anchor: middle;
+    dominant-baseline: middle;
+  }
+`;
---
+++ packages/openbridge-webcomponents/src/skipper/vessel-view/vessel.stories.ts
@@ -0,0 +1,47 @@
+import type {Meta, StoryObj} from '@storybook/web-components-vite';
+import {widthDecorator} from '../../storybook-util.js';
+import {Vessel} from './vessel.js';
+import './vessel.js';
+
+const meta: Meta<typeof Vessel> = {
+  title: 'INSTRUMENT/Vessel',
+  tags: ['autodocs', '6.0'],
+  component: 'ob-vessel',
+  parameters: {
+    actions: {
+      handles: ['click'],
+    },
+  },
+  args: {
+    width: 512,
+  },
+  argTypes: {
+    width: {control: {type: 'range', min: 32, max: 1028, step: 1}},
+  },
+  decorators: [widthDecorator],
+} satisfies Meta<Vessel>;
+
+export default meta;
+type Story = StoryObj<Vessel>;
+
+export const Vessel_tanker: Story = {
+  args: {
+    // width: 512,
+    vesselHeight: 40,
+    bowToCCRP: 50,
+    sternToCCRP: 50,
+    sensorToCCRP: 0,
+    sensorHeightOverKeel: 50,
+    portToCCRP: 15,
+    starboardToCCRP: 15,
+    sensorPortStarboardOffset: 0,
+    sideTopDownViewToggle: false,
+    toggleSLAndDLSensor: true,
+  },
+};
+
+export const Vessel_cruiser: Story = {
+  args: {
+    width: 512,
+  },
+};
