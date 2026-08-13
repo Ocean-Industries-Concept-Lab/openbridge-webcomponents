b2a4b2b1843cae27e105f3debf5a25109fe6a62f
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2026-08-13 11:42:53 +0200

    test

--- packages/openbridge-webcomponents/src/palettes/blinking.ts
+++ packages/openbridge-webcomponents/src/palettes/blinking.ts
@@ -2,37 +2,46 @@ const CRITICAL_PERIOD = 1000; // ms
 const ALARM_PERIOD = 2000; // ms
 const WARNING_PERIOD = 4000; // ms
 const LOW_PERIOD = 8000; // ms
+const BLINK_OFF_DURATION = 250; // ms
 
 function blinkingInstall(
   el: HTMLElement,
   period: number,
+  offset: number,
   label: string
 ): () => void {
-  const anim = el.animate(
-    [
-      {[label + '-on']: 1, [label + '-off']: 0},
-      {[label + '-on']: 0, [label + '-off']: 1},
-    ],
-    {duration: period, iterations: Infinity, easing: 'steps(2, jump-none)'}
-  );
-  anim.startTime = 0; // Common time origin for all animations, so they blink in sync.
+  const blinkOffDuration = BLINK_OFF_DURATION / period;
+  const frames: Keyframe[] = [
+    {[label + '-on']: 0, [label + '-off']: 1, easing: 'step-end'},
+    {
+      [label + '-on']: 1,
+      [label + '-off']: 0,
+      offset: blinkOffDuration,
+      easing: 'step-end',
+    },
+    {[label + '-on']: 1, [label + '-off']: 0},
+  ];
+  const anim = el.animate(frames, {duration: period, iterations: Infinity});
+  // Common time origin for all animations, so they blink in sync. The offset is
+  // negative so the animation starts already that far into its period.
+  anim.startTime = offset;
   return () => anim.cancel();
 }
 
 export function blinkingCritical(el: HTMLElement): () => void {
-  return blinkingInstall(el, CRITICAL_PERIOD, '--critical-blink');
+  return blinkingInstall(el, CRITICAL_PERIOD, 0, '--critical-blink');
 }
 
 export function blinkingAlarm(el: HTMLElement): () => void {
-  return blinkingInstall(el, ALARM_PERIOD, '--alarm-blink');
+  return blinkingInstall(el, ALARM_PERIOD, 250, '--alarm-blink');
 }
 
 export function blinkingWarning(el: HTMLElement): () => void {
-  return blinkingInstall(el, WARNING_PERIOD, '--warning-blink');
+  return blinkingInstall(el, WARNING_PERIOD, 500, '--warning-blink');
 }
 
 export function blinkingLow(el: HTMLElement): () => void {
-  return blinkingInstall(el, LOW_PERIOD, '--low-blink');
+  return blinkingInstall(el, LOW_PERIOD, 750, '--low-blink');
 }
 
 export function blinkingAll(el: HTMLElement): () => void {
